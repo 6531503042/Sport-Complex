03440e4544e869cea68112c6fa4f624247075d13
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 15:18:49 +0700

    User: Enchance userEntity

--- backend/modules/user/userEntity.go
+++ backend/modules/user/userEntity.go
@@ -9,27 +9,26 @@ import (
 type (
 
 	User struct {
-		Id	primitive.ObjectID	`json:"_id" bson:"_id,omitempty"`
-		Email string `json:"email" bson:"email"`
-		Username string `json:"username bson:"username"`
-		Password string `json:"password" bson:"password"`
-		CreatedAt time.Time `json:"created_at" bson:"created_at"`
-		UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
-		RoleCode []UserRole  `bson:"role_code"`
+		Id        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+		Email     string             `json:"email" bson:"email"`
+		Name      string             `json:"name" bson:"name"`
+		Password  string             `json:"password" bson:"password"`
+		CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+		UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+		RoleCode  []UserRole         `json:"role_code" bson:"role_code"`
 	}
 
 	UserRole struct {
 		RoleTitle string `json:"role_title" bson:"role_title"`
-		RoleCode int `json:"role_code" bson:"role_code"`
-
+		RoleCode  int    `json:"role_code" bson:"role_code"`
 	}
 
 	UserProfileBson struct {
-		Id primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-		Email string `json:"email" bson:"email"`
-		Username string `json:"username bson:"username"`
-		Password string `json:"password" bson:"password"`
-		CreatedAt time.Time `json:"created_at" bson:"created_at"`
-		UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
+		Id        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+		Email     string             `json:"email" bson:"email"`
+		Name      string             `json:"name" bson:"name"`
+		Password  string             `json:"password" bson:"password"`
+		CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+		UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 	}
 )
