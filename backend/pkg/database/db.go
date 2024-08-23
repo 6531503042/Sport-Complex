0d68f9ad7c7f652320a3ae112138bba25d9aa582
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 15:17:03 +0700

    Database: Added db.go for main database mongodb Connect

---
+++ backend/pkg/database/db.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func DbConnect (pctx context.Context, cfg *config,Config) *mongo.Client {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.Db.Url))
+	if err != nil {
+		log.Fatalf("Error: Connect to database error: %s", err.Error())
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatalf("Error: Ping to database error: %s", err.Error())
+	}
+
+	return client
+}
