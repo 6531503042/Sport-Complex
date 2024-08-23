c9625a9a95fa4500c9c3f16b44a936a03199da14
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 15:18:31 +0700

    HttpHandlers: Added user_http_handlers

---
+++ backend/modules/user/handlers/userHttpHandler.go
@@ -0,0 +1,12 @@
+package userhandler
+
+// type (
+// 	PlayerHttpHandlerService interface {
+// 		CreateUser (c echo.Context) error
+// 	}
+
+// 	userHttpHandler struct {
+// 		cfg		*config.Config
+// 	}
+// )
+
