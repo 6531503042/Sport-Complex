eb3fb654b2b1f9a884a196a2e9379e52d1ea7fb8
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 15:15:59 +0700

    Utils: Added Mongo Helper to convert Object

---
+++ backend/pkg/utils/mongo.go
@@ -0,0 +1,8 @@
+package utils
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+func ConvertToObjectId (id string) primitive.ObjectID {
+	objectId, _ := primitive.ObjectIDFromHex(id)
+	return objectId
+}
