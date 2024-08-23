0d148a2ebe2363075725b06f11e357aae8b0d949
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 20:09:14 +0700

    Resquest: Added custom resquest binding

---
+++ backend/pkg/request/request.go
@@ -0,0 +1,38 @@
+package request
+
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo"
+)
+
+type (
+	contextWrapperService interface {
+		Bind(data any) error
+	}
+
+	contextWrapper struct {
+		Context   echo.Context
+		validator *validator.Validate
+	}
+)
+
+func ContextWrapper(ctx echo.Context) contextWrapperService {
+	return &contextWrapper{
+		Context:   ctx,
+		validator: validator.New(),
+	}
+}
+
+func (c *contextWrapper) Bind(data any) error {
+	if err := c.Context.Bind(data); err != nil {
+		log.Printf("Error: Bind: %s", err.Error())
+	}
+
+	if err := c.validator.Struct(data); err != nil {
+		log.Printf("Error: Struct: %s", err.Error())
+	}
+
+	return nil
+}
