d04e9b0a0e4dc80879f6134474aa1d948162cd1d
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 20:10:06 +0700

    HttpHandler: Added userHttpHandler

--- backend/modules/user/handlers/userHttpHandler.go
+++ backend/modules/user/handlers/userHttpHandler.go
@@ -1,12 +1,71 @@
 package handlers
 
-// type (
-// 	PlayerHttpHandlerService interface {
-// 		CreateUser (c echo.Context) error
-// 	}
+import (
+	"context"
+	"main/config"
+	"main/modules/user"
+	"main/modules/user/usecase"
+	"main/pkg/request"
+	"main/pkg/response"
+	"net/http"
+	"strings"
 
-// 	userHttpHandler struct {
-// 		cfg		*config.Config
-// 	}
-// )
+	"github.com/labstack/echo"
+) 
 
+type (
+	PlayerHttpHandlerService interface {
+
+	}
+
+	userHttpHandler struct {
+		cfg		*config.Config
+		userUsecase usecase.UserUsecaseService
+	}
+)
+
+func NewUserHttpHandler(cfg *config.Config, userUsecase usecase.UserUsecaseService) PlayerHttpHandlerService {
+	return &userHttpHandler{
+		userUsecase: userUsecase,
+	}
+}
+
+func (h *userHttpHandler) createUser(c echo.Context) error {
+	ctx := context.Background()
+
+	// Use the custom binding
+	wrapper := request.ContextWrapper(c)
+
+	req := new(user.CreateUserReq)
+
+	// Handle binding errors
+	if err := wrapper.Bind(req); err != nil {
+		return response.ErrResponse(c, http.StatusBadRequest, "Invalid request payload")
+	}
+
+	// Call the Usecase to create a user
+	res, err := h.userUsecase.CreateUser(ctx, req)
+	if err != nil {
+		return response.ErrResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	return response.SuccessResponse(c, http.StatusCreated, res)
+}
+
+func (h *userHttpHandler) FindOneUserProfile(c echo.Context) error {
+	// Use the context from the request
+	ctx := c.Request().Context()
+
+	// Extract and sanitize the user ID from the URL parameter
+	userId := strings.TrimPrefix(c.Param("user_id"), "users:")
+
+	// Fetch the user profile
+	res, err := h.userUsecase.FindOneUserProfile(ctx, userId)
+	if err != nil {
+		// Return an error response
+		return response.ErrResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	// Return a successful response with the user profile
+	return response.SuccessResponse(c, http.StatusOK, res)
+}
