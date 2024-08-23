f61e9a9f64cc0e931b060b15549724a1a58c4130
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-23 20:09:01 +0700

    Response: Added custom response binding

---
+++ backend/pkg/response/response.go
@@ -0,0 +1,26 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type (
+	MsgResponse struct {
+		Message string `json:"message"`
+	}
+)
+
+func ErrResponse(c echo.Context, statusCode int, message string) error {
+	if statusCode == http.StatusInternalServerError {
+		// log error
+		c.Logger().Errorf("error: %s", message)
+	}
+
+	return c.JSON(statusCode, &MsgResponse{Message: message})
+}
+
+func SuccessResponse(c echo.Context, statusCode int, data any) error {
+	return c.JSON(statusCode, data)
+}
