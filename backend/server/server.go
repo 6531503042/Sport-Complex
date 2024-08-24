9d5bfe9c3492d4b782cf99ef434691431c8d56cb
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-24 20:42:39 +0700

    Server: Added start func

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"context"
+	"main/config"
+
 	"github.com/labstack/echo"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -9,5 +12,40 @@ type (
 	server struct {
 		app		*echo.Echo
 		db		*mongo.Client
+		cfg		*config.Config
 	}
 )
+
+func Start(pctx context.Context, cfg *config.Config, db *mongo.Client) {
+	s := &server{
+		app:	echo.New(),
+		db:		db,
+		cfg: 	cfg,
+	}
+
+	//Cors
+	// s.app.Use(middleware.CORS
+
+
+}
+
+// func (s *server) setupMiddleware() {
+// 	// Request Timeout
+// 	s.app.Use(middleware.TimeoutWithConfig(middleware.TimeoutConfig{
+// 		Skipper:      middleware.DefaultSkipper,
+// 		ErrorMessage: "Error: Request Timeout",
+// 		Timeout:      30 * time.Second,
+// 	}))
+
+// 	// CORS
+// 	s.app.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+// 		Skipper:      middleware.DefaultSkipper,
+// 		AllowOrigins: []string{"*"},
+// 		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.PATCH, echo.DELETE},
+// 	}))
+
+// 	// Body Limit
+// 	s.app.Use(middleware.BodyLimit("10M"))
+
+// 	// Additional middleware can be configured here...
+// }
