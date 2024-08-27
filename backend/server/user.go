2e0f4d57af0239b9053c0e51429c4ce4f410bd1b
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-27 22:43:59 +0700

    Server: Add user_server

---
+++ backend/server/user.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"main/modules/user/handlers"
+	"main/modules/user/repository"
+	"main/modules/user/usecase"
+)
+func (s *server) userService() {
+	repo := repository.NewUserRepository(s.db)
+	userUsecase := usecase.NewUserUsecase(repo)
+	httpHandler := handlers.NewUserHttpHandler(s.cfg, userUsecase)
+	user := s.app.Group("/user_v1")
+
+	// Health check endpoint
+	// user.GET("", s.healthCheckService)
+
+	// User endpoints
+	user.POST("/users/register", httpHandler.CreateUser)
+	user.GET("/users/:user_id", httpHandler.FindOneUserProfile)
+}
+
