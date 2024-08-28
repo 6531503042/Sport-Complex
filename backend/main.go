b72792043250c58587ffbde10d7327bd48e7dfed
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-28 15:00:34 +0700

    Main: Add start server and db connect

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"main/config"
+	"main/pkg/database"
+	"main/server"
+	"os"
+)
 
+func main() {
 	// ctx := context.Background()
+	ctx := context.Background()
+
+	// Initialize config
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		return os.Args[1]
+	}())
+
+	db := database.DbConn(ctx, &cfg)
+	defer db.Disconnect(ctx)
+
+	//Start server
+	server.Start(ctx, &cfg, db)
 
 	
 	
