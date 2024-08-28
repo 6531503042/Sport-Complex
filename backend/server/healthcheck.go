7cce8201eded5665f61c715ab52091800eb387c5
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-28 11:35:00 +0700

    Server: Added healthcheck_server

---
+++ backend/server/healthcheck.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"main/pkg/response"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type (
+	healthCheck struct {
+		App string `json:"app"`
+		Status string `json:"status"`
+	}
+)
+
+func (s *server) healthCheckService (c echo.Context) error {
+	return response.SuccessResponse(c, http.StatusOK, &healthCheck{
+		App: s.cfg.App.Name,
+		Status: "OK",
+	})
+}
