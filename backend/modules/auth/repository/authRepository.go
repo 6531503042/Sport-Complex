23b26615624443cf2e795c38637e08518cb0384a
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-29 20:34:27 +0700

    Repository: Added Auth_Repo

--- backend/modules/auth/repository/authRepository.go
+++ backend/modules/auth/repository/authRepository.go
@@ -1,25 +1,54 @@
-// package repository
+package repository
 
-// import (
-// 	"context"
+import (
+	"context"
+	"errors"
+	"log"
+	"main/modules/auth"
+	"main/pkg/utils"
+	"time"
 
-// 	"go.mongodb.org/mongo-driver/mongo"
-// )
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-// type (
-// 	AuthRepositoryService interface {
+type (
+	AuthRepositoryService interface {
+		InsertOneUserCredential (pctx context.Context, req *auth.Credential) (primitive.ObjectID, error)
+	}
 
-// 	}
+	authRepository struct {
+		db *mongo.Client
+	}
+)
 
-// 	authRepository struct {
-// 		db *mongo.Client
-// 	}
-// )
+func NewAuthRepository(db *mongo.Client) AuthRepositoryService {
+	return &authRepository{db: db}
+}
 
-// func NewAuthRepository(db *mongo.Client) AuthRepositoryService {
-// 	return &authRepository{db: db}
-// }
+func (r * authRepository) authDbConn(pctx context.Context) *mongo.Database {
+	return r.db.Database("auth_db")
+}
 
-// func (r * authRepository) authDbConn(pctx context.Context) *mongo.Database {
-// 	return r.db.Database("auth_db")
-// }
+func (r * authRepository) InsertOneUserCredential (pctx context.Context, req *auth.Credential) (primitive.ObjectID, error) {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	db := r.authDbConn(ctx)
+	col := db.Collection("auth")
+
+	req.CreatedAt = utils.LocalTime()
+	req.UpdatedAt = utils.LocalTime()
+
+	result, err := col.InsertOne(ctx, req)
+	if err != nil {
+		log.Printf("Error: InsertOneUserCredential: %s", err.Error())
+		return primitive.NilObjectID, errors.New("error: insert one user credential failed")
+	}
+
+	userId, ok := result.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return primitive.NilObjectID, errors.New("error: insert one user credential failed")
+	}
+	return userId, nil
+}
