c27a48025d4ca03a1a1656687eff14707a57618b
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-31 16:13:12 +0700

    User: Added usecase for user addition

--- backend/modules/user/usecase/userUsecase.go
+++ backend/modules/user/usecase/userUsecase.go
@@ -3,7 +3,9 @@ package usecase
 import (
 	"context"
 	"errors"
+	"log"
 	"main/modules/user"
+	userPb "main/modules/user/proto"
 	"main/modules/user/repository"
 	"main/pkg/utils"
 	"time"
@@ -16,6 +18,8 @@ type (
 	UserUsecaseService interface {
         CreateUser (pctx context.Context, req *user.CreateUserReq) (*user.UserProfile, error)
         FindOneUserProfile (pctx context.Context, userId string) (*user.UserProfile, error)
+        FindOneUserCredential(pctx context.Context, password, email string) (*userPb.UserProfile, error)
+		FindOneUserProfileToRefresh(pctx context.Context, userId string) (*userPb.UserProfile, error)
 	}
 
 	userUsecase struct {
@@ -78,3 +82,55 @@ func (u * userUsecase) FindOneUserProfile (pctx context.Context, userId string) 
         UpdatedAt: result.UpdatedAt.In(loc),
     }, nil
 }
+
+func (u *userUsecase) FindOneUserCredential(pctx context.Context, password, email string) (*userPb.UserProfile, error) {
+    result, err := u.userRepository.FindOneUserCredential(pctx, email)
+    if err != nil {
+        return nil, err
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(password)); err != nil {
+        log.Printf("Error: FindOneUserCredential: %s",err.Error())
+        return nil, errors.New("error: wrong password")
+    }
+
+    roleCode := 0
+    for _, v := range result.UserRoles {
+        roleCode = v.RoleCode
+    }
+
+    loc, _ := time.LoadLocation("Asia/Bangkok")
+
+    return &userPb.UserProfile{
+        Id:        result.Id.Hex(),
+        Email:     result.Email,
+        Name:      result.Name,
+        RoleCode:  int32(roleCode),
+        CreatedAt: result.CreatedAt.In(loc).String(),
+        UpdatedAt: result.CreatedAt.In(loc).String(),
+    }, nil
+}
+
+func (u *userUsecase) FindOneUserProfileToRefresh(pctx context.Context, userId string) (*userPb.UserProfile, error) {
+    result, err := u.userRepository.FindOneUserProfileRefresh(pctx, userId)
+    if err != nil {
+        return nil, err
+    }
+
+    roleCode := 0
+    for _, v := range result.UserRoles {
+        roleCode = v.RoleCode
+    }
+
+    loc, _ := time.LoadLocation("Asia/Bangkok")
+
+    return &userPb.UserProfile{
+        Id:        result.Id.Hex(),
+        Email:     result.Email,
+        Name:      result.Name,
+        RoleCode:  int32(roleCode),
+        CreatedAt: result.CreatedAt.In(loc).String(),
+        UpdatedAt: result.CreatedAt.In(loc).String(),
+    }, nil
+}
+
