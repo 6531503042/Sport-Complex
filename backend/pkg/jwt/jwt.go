1e8fe60729a4f8a98b31ea17a508d740604394cc
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-08-31 13:56:03 +0700

    Pkg: Added Jwt Function for Auth

---
+++ backend/pkg/jwt/jwt.go
@@ -0,0 +1,182 @@
+package jwt
+
+import (
+	"context"
+	"errors"
+	"math"
+	"sync"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"google.golang.org/grpc/metadata"
+)
+
+type (
+	AuthFactory interface {
+		SignToken() (string, error)
+	}
+
+	Claim struct {
+		UserId   string `json:"userId"`
+		RoleCode int    `json:"role_code"`
+	}
+
+	AuthMapClaims struct {
+		*Claim
+		jwt.RegisteredClaims
+	}
+
+	authConcrete struct {
+		Secret []byte
+		Claims *AuthMapClaims `json:"claims"`
+	}
+
+	accessToken struct {
+		*authConcrete
+	}
+
+	refreshToken struct {
+		*authConcrete
+	}
+
+	apiKey struct {
+		*authConcrete
+	}
+)
+
+func (a *authConcrete) SignToken() (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.Claims)
+	return token.SignedString(a.Secret)
+}
+
+func now() time.Time {
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	return time.Now().In(loc)
+}
+
+func jwtTimeDurationCal(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(now().Add(time.Duration(t * int64(math.Pow10(9)))))
+}
+
+func jwtTimeRepeatAdapter(t int64) *jwt.NumericDate {
+	return jwt.NewNumericDate(time.Unix(t, 0))
+}
+
+func NewAccessToken(secret string, expiredAt int64, claim *Claim) AuthFactory {
+	return &accessToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claim: claim,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "bengi.com",
+					Subject:   "access-token",
+					Audience:  []string{"bengi.com"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func NewRefreshToken(secret string, expiredAt int64, claim *Claim) AuthFactory {
+	return &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claim: claim,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "bengi.com",
+					Subject:   "refresh-token",
+					Audience:  []string{"bengi.com"},
+					ExpiresAt: jwtTimeDurationCal(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func ReloadToken(secret string, expiredAt int64, claim *Claim) string {
+	obj := &refreshToken{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				Claim: claim,
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "bengi.com",
+					Subject:   "refresh-token",
+					Audience:  []string{"bengi.com"},
+					ExpiresAt: jwtTimeRepeatAdapter(expiredAt),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+
+	tokenString, err := obj.SignToken()
+	if err != nil {
+		return ""
+	}
+	return tokenString
+}
+
+func NewApiKey(secret string) AuthFactory {
+	return &apiKey{
+		authConcrete: &authConcrete{
+			Secret: []byte(secret),
+			Claims: &AuthMapClaims{
+				RegisteredClaims: jwt.RegisteredClaims{
+					Issuer:    "bengi.com",
+					Subject:   "api-key",
+					Audience:  []string{"bengi.com"},
+					ExpiresAt: jwtTimeDurationCal(31560000),
+					NotBefore: jwt.NewNumericDate(now()),
+					IssuedAt:  jwt.NewNumericDate(now()),
+				},
+			},
+		},
+	}
+}
+
+func ParseToken(secret string, tokenString string) (*AuthMapClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &AuthMapClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("error: unexpected signing method")
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		if errors.Is(err, jwt.ErrTokenMalformed) {
+			return nil, errors.New("error: token format is invalid")
+		} else if errors.Is(err, jwt.ErrTokenExpired) {
+			return nil, errors.New("error: token is expired")
+		} else {
+			return nil, errors.New("error: token is invalid")
+		}
+	}
+
+	if claims, ok := token.Claims.(*AuthMapClaims); ok {
+		return claims, nil
+	} else {
+		return nil, errors.New("error: claims type is invalid")
+	}
+}
+
+var apiKeyInstant string
+var once sync.Once
+
+func SetApiKey(secret string) {
+	once.Do(func() {
+		apiKeyInstant, _ = NewApiKey(secret).SignToken()
+	})
+}
+
+func SetApiKeyInContext(pctx *context.Context) {
+	*pctx = metadata.NewOutgoingContext(*pctx, metadata.Pairs("auth", apiKeyInstant))
+}
+
