201680b46f2780872df3a1c82cb19ecc98314f6e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-01 18:23:19 +0700

    Middleware: Added usecase middleware

--- backend/modules/middleware/middlewareUsecase/middlewareUsecase.go
+++ backend/modules/middleware/middlewareUsecase/middlewareUsecase.go
@@ -1,11 +1,16 @@
 package middlewareusecase
 
 import (
+	"main/config"
 	middlewareRepository "main/modules/middleware/middlewareRepository"
+	"main/pkg/jwt"
+
+	"github.com/labstack/echo/v4"
 )
 
 type (
 	MiddlewareUsecaseService interface {
+		JwtAuthorization(c echo.Context, cfg *config.Config, accessToken string) (echo.Context, error)
 	}
 
 	middlewareUsecase struct {
@@ -16,4 +21,21 @@ type (
 
 func NewMiddlewareUsecase(middlewareRepository middlewareRepository.MiddlewareRepositoryService) MiddlewareUsecaseService {
 	return &middlewareUsecase{middlewareRepository}
+}
+
+func (u *middlewareUsecase) JwtAuthorization(c echo.Context, cfg *config.Config, accessToken string) (echo.Context, error) {
+	ctx := c.Request().Context()
+	claims, err := jwt.ParseToken(cfg.Jwt.AccessSecretKey, accessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := u.middlewareRepository.AccessTokenSearch(ctx, cfg.Grpc.AuthUrl, accessToken); err != nil {
+		return nil, err
+	}
+
+	c.Set("user_id", claims.UserId)
+	c.Set("role_code", claims.RoleCode)
+
+	return c, nil
 }
