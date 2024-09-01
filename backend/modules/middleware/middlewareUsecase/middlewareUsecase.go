70030c2a800636aa17a476df5b91fdd27549ca58
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-01 18:40:15 +0700

    Middleware: Completed usecase middle

--- backend/modules/middleware/middlewareUsecase/middlewareUsecase.go
+++ backend/modules/middleware/middlewareUsecase/middlewareUsecase.go
@@ -1,9 +1,12 @@
 package middlewareusecase
 
 import (
+	"errors"
+	"log"
 	"main/config"
 	middlewareRepository "main/modules/middleware/middlewareRepository"
 	"main/pkg/jwt"
+	"main/pkg/rbac"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,12 +14,13 @@ import (
 type (
 	MiddlewareUsecaseService interface {
 		JwtAuthorization(c echo.Context, cfg *config.Config, accessToken string) (echo.Context, error)
+		RbacAuthorization(c echo.Context, cfg *config.Config, expected []int) (echo.Context, error)
+		UserIdParamValidation(c echo.Context) (echo.Context, error)
 	}
 
 	middlewareUsecase struct {
 		middlewareRepository middlewareRepository.MiddlewareRepositoryService
 	}
-	
 )
 
 func NewMiddlewareUsecase(middlewareRepository middlewareRepository.MiddlewareRepositoryService) MiddlewareUsecaseService {
@@ -36,6 +40,47 @@ func (u *middlewareUsecase) JwtAuthorization(c echo.Context, cfg *config.Config,
 
 	c.Set("user_id", claims.UserId)
 	c.Set("role_code", claims.RoleCode)
+
+	return c, nil
+}
+
+//Set authorization for rbac.
+func (u *middlewareUsecase) RbacAuthorization(c echo.Context, cfg *config.Config, expected []int) (echo.Context, error) {
+	ctx := c.Request().Context()
+
+	userRoleCode := c.Get("role_code").(int)
+
+	rolesCount, err := u.middlewareRepository.RolesCount(ctx, cfg.Grpc.AuthUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	userRoleBinary := rbac.IntToBinary(userRoleCode, int(rolesCount))
+
+	for i := 0; i < int(rolesCount); i++ {
+		if userRoleBinary[i]&expected[i] == 1 {
+			return c, nil
+		}
+	}
+
+	return nil, errors.New("error: permission denied")
+}
+
+func (u *middlewareUsecase) UserIdParamValidation(c echo.Context) (echo.Context, error) {
+	userIdReq := c.Param("user_id")
+	userIdToken := c.Get("user_id")
+
+	if userIdToken == nil {
+		log.Printf("Error: user_id not found")
+		return nil, errors.New("error: user_id is required")
+	}
+
+	userIdTokenStr := userIdToken.(string)
+
+	if userIdTokenStr != userIdReq {
+		log.Printf("Error: user_id not match, user_id_req: %s, user_id_token: %s", userIdReq, userIdTokenStr)
+		return nil, errors.New("error: user_id not match")
+	}
 
 	return c, nil
 }
