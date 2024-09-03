08462040282b2efbbe9e568da5abb129f515f31b
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-03 22:03:47 +0700

    Handler: Added echo handler for user.

--- backend/modules/user/handlers/userHttpHandler.go
+++ backend/modules/user/handlers/userHttpHandler.go
@@ -1,12 +1,71 @@
 package handlers
 
-// type (
-// 	PlayerHttpHandlerService interface {
-// 		CreateUser (c echo.Context) error
-// 	}
+import (
+	"log"
+	"main/config"
+	"main/modules/user"
+	"main/modules/user/usecase"
+	"main/pkg/request"
+	"main/pkg/response"
+	"net/http"
 
-// 	userHttpHandler struct {
-// 		cfg		*config.Config
-// 	}
-// )
+	"github.com/labstack/echo/v4"
+)
 
+type (
+	userHttpHandler struct {
+		cfg         *config.Config
+		userUsecase usecase.UserUsecaseService
+	}
+)
+
+// NewUserHttpHandler initializes and returns a userHttpHandler
+func NewUserHttpHandler(cfg *config.Config, userUsecase usecase.UserUsecaseService) *userHttpHandler {
+	return &userHttpHandler{
+		cfg:         cfg,
+		userUsecase: userUsecase,
+	}
+}
+
+func (h *userHttpHandler) CreateUser(c echo.Context) error {
+
+	log.Println("Received request to create user")
+	// Use the context from the request
+	ctx := c.Request().Context()
+
+	// Use the custom binding
+	wrapper := request.ContextWrapper(c)
+
+	req := new(user.CreateUserReq)
+
+	// Handle binding errors
+	if err := wrapper.Bind(req); err != nil {
+		return response.ErrResponse(c, http.StatusBadRequest, "Invalid request payload")
+	}
+
+	// Call the Usecase to create a user
+	res, err := h.userUsecase.CreateUser(ctx, req)
+	if err != nil {
+		return response.ErrResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	return response.SuccessResponse(c, http.StatusCreated, res)
+}
+
+func (h *userHttpHandler) FindOneUserProfile(c echo.Context) error {
+	// Use the context from the request
+	ctx := c.Request().Context()
+
+	// Extract the user ID from the URL parameter
+	userId := c.Param("user_id")
+
+	// Fetch the user profile
+	res, err := h.userUsecase.FindOneUserProfile(ctx, userId)
+	if err != nil {
+		// Return an error response
+		return response.ErrResponse(c, http.StatusBadRequest, err.Error())
+	}
+
+	// Return a successful response with the user profile
+	return response.SuccessResponse(c, http.StatusOK, res)
+}
