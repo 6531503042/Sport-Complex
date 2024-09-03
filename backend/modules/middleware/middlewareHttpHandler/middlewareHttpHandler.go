09e5e69eebee56180d7228640374a8d01e1cf874
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-03 22:04:21 +0700

    Midleware: Added echo http middleware.

--- backend/modules/middleware/middlewareHttpHandler/middlewareHttpHandler.go
+++ backend/modules/middleware/middlewareHttpHandler/middlewareHttpHandler.go
@@ -1,17 +1,69 @@
 package middlewarehttphandler
 
-import middlewareusecase "main/modules/middleware/middlewareUsecase"
+import (
+	"main/config"
+	middlewareusecase "main/modules/middleware/middlewareUsecase"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 type (
-	middlewareHttpHandlerService interface {
-
+	MiddlewareHttpHandlerService interface {
+		JwtAuthorizationMiddleware(cfg *config.Config) echo.MiddlewareFunc
+		RbacAuthorizationMiddleware(cfg *config.Config, expected []int) echo.MiddlewareFunc
+		UserIdParamValidationMiddleware() echo.MiddlewareFunc
 	}
 
-	middlewareHttpHandler struct {
+	MiddlewareHttpHandler struct {
 		middlewareUsecase middlewareusecase.MiddlewareUsecaseService
 	}
 )
 
-func NewMiddlewareUsecase(middlewareUsecase middlewareusecase.MiddlewareUsecaseService) middlewareHttpHandlerService {
-	return &middlewareHttpHandler{middlewareUsecase: }
+func NewMiddlewareHttpHandler(middlewareUsecase middlewareusecase.MiddlewareUsecaseService) MiddlewareHttpHandlerService {
+	return &MiddlewareHttpHandler{middlewareUsecase}
+}
+
+func (m *MiddlewareHttpHandler) JwtAuthorizationMiddleware(cfg *config.Config) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			accessToken := c.Request().Header.Get("Authorization")
+			if accessToken == "" {
+				return echo.NewHTTPError(http.StatusUnauthorized, "missing access token")
+			}
+
+			_, err := m.middlewareUsecase.JwtAuthorization(c, cfg, accessToken)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+			}
+
+			return next(c)
+		}
+	}
+}
+
+func (m *MiddlewareHttpHandler) RbacAuthorizationMiddleware(cfg *config.Config, expected []int) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			_, err := m.middlewareUsecase.RbacAuthorization(c, cfg, expected)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusForbidden, err.Error())
+			}
+
+			return next(c)
+		}
+	}
+}
+
+func (m *MiddlewareHttpHandler) UserIdParamValidationMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			_, err := m.middlewareUsecase.UserIdParamValidation(c)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+			}
+
+			return next(c)
+		}
+	}
 }
