9c33f13f13ccc7dcad74fb368a06f7b7d588ff1c
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-03 22:01:34 +0700

    User: Added usecase for user.

--- backend/modules/user/usecase/userUsecase.go
+++ backend/modules/user/usecase/userUsecase.go
@@ -3,7 +3,9 @@ package usecase
 import (
 	"context"
 	"errors"
+	"log"
 	"main/modules/user"
+	userPb "main/modules/user/proto"
 	"main/modules/user/repository"
 	"main/pkg/utils"
 	"time"
@@ -14,7 +16,10 @@ import (
 type (
 
 	UserUsecaseService interface {
-        createUser (pctx context.Context, req *user.CreateUserReq) (*user.UserProfile, error)
+        CreateUser (pctx context.Context, req *user.CreateUserReq) (*user.UserProfile, error)
+        FindOneUserProfile (pctx context.Context, userId string) (*user.UserProfile, error)
+        FindOneUserCredential(pctx context.Context, password, email string) (*userPb.UserProfile, error)
+		FindOneUserProfileToRefresh(pctx context.Context, userId string) (*userPb.UserProfile, error)
 	}
 
 	userUsecase struct {
@@ -28,7 +33,7 @@ func NewUserUsecase(userRepository repository.UserRepositoryService) UserUsecase
 	}
 }
 
-func (u *userUsecase) createUser(pctx context.Context, req *user.CreateUserReq) (*user.UserProfile, error) {
+func (u *userUsecase) CreateUser(pctx context.Context, req *user.CreateUserReq) (*user.UserProfile, error) {
     // Check if the user with the given email or name already exists
     if !u.userRepository.IsUniqueUser(pctx, req.Email, req.Name) {
         return nil, errors.New("error: email or name already existing")
@@ -37,7 +42,7 @@ func (u *userUsecase) createUser(pctx context.Context, req *user.CreateUserReq) 
     // Hash the password
     hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
     if err != nil {
-        return nil, errors.New("Error: failed to hash password")
+        return nil, errors.New("error: failed to hash password")
     }
 
     // Insert the new user
@@ -77,3 +82,55 @@ func (u * userUsecase) FindOneUserProfile (pctx context.Context, userId string) 
         UpdatedAt: result.UpdatedAt.In(loc),
     }, nil
 }
+
+func (u *userUsecase) FindOneUserCredential(pctx context.Context, password, email string) (*userPb.UserProfile, error) {
+    result, err := u.userRepository.FindOneUserCredential(pctx, email)
+    if err != nil {
+        return nil, err
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(result.Password), []byte(password)); err != nil {
+        log.Printf("Error: FindOneUserCredential: %s",err.Error())
+        return nil, errors.New("error: wrong password")
+    }
+
+    roleCode := 0
+    for _, v := range result.UserRoles {
+        roleCode = v.RoleCode
+    }
+
+    loc, _ := time.LoadLocation("Asia/Bangkok")
+
+    return &userPb.UserProfile{
+        Id:        result.Id.Hex(),
+        Email:     result.Email,
+        Name:      result.Name,
+        RoleCode:  int32(roleCode),
+        CreatedAt: result.CreatedAt.In(loc).String(),
+        UpdatedAt: result.CreatedAt.In(loc).String(),
+    }, nil
+}
+
+func (u *userUsecase) FindOneUserProfileToRefresh(pctx context.Context, userId string) (*userPb.UserProfile, error) {
+    result, err := u.userRepository.FindOneUserProfileRefresh(pctx, userId)
+    if err != nil {
+        return nil, err
+    }
+
+    roleCode := 0
+    for _, v := range result.UserRoles {
+        roleCode = v.RoleCode
+    }
+
+    loc, _ := time.LoadLocation("Asia/Bangkok")
+
+    return &userPb.UserProfile{
+        Id:        result.Id.Hex(),
+        Email:     result.Email,
+        Name:      result.Name,
+        RoleCode:  int32(roleCode),
+        CreatedAt: result.CreatedAt.In(loc).String(),
+        UpdatedAt: result.CreatedAt.In(loc).String(),
+    }, nil
+}
+
