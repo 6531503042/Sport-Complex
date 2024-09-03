f8c827bae753458d023b9e6c61204f554528c75d
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-03 22:03:58 +0700

    Handlers: Added grpc handler for user.

---
+++ backend/modules/user/handlers/userGrpcHandler.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"context"
+	userPb "main/modules/user/proto"
+	"main/modules/user/usecase"
+)
+
+type (
+	userGrpcHandler struct {
+		userUsecase usecase.UserUsecaseService
+		userPb.UnimplementedUserGrpcServiceServer
+	}
+)
+
+func NewUserGrpcHandler(userUsecase usecase.UserUsecaseService) *userGrpcHandler {
+	return &userGrpcHandler{userUsecase: userUsecase}
+}
+
+func (g *userGrpcHandler) CredentialSearch (ctx context.Context, req *userPb.CredentialSearchReq) (*userPb.UserProfile, error) {
+	return g.userUsecase.FindOneUserCredential(ctx, req.Password, req.Email)
+}
+
+func (g *userGrpcHandler) FindOneUserProfileRefresh (ctx context.Context, req *userPb.FindOneUserProfileToRefreshReq) (*userPb.UserProfile, error) {
+	return g.userUsecase.FindOneUserProfileToRefresh(ctx, req.UserId)
+}
