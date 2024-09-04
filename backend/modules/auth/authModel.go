005c126a75f3f2731277a016ede7d3e52c976204
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-04 20:32:50 +0700

    Update authModel.go

--- backend/modules/auth/authModel.go
+++ backend/modules/auth/authModel.go
@@ -7,21 +7,21 @@ import (
 
 type (
 	UserLoginReq struct {
-		Email    string `json:"email" form:"email" validate: "required, email, max=255"`
-		Password string `json:"password" form:"password" validate: "required, max=32"`
+		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
+		Password string `json:"password" form:"password" validate:"required,max=32"`
 	}
 
 	UserLoginRes struct {
-		CredentialId string `json:"credential_id" form:"credential_id" validate: "required, max=60"`
+		CredentialId string `json:"credential_id" form:"credential_id" validate:"required,max=64"`
 	}
 
 	RefreshTokenReq struct {
-		CredentialId string `json:"credential_id" form:"credential_id" validate: "required, max=60"`
-		RefreshToken string `json:"refresh_token" form:"refresh_token" validate: "required, max=500"`
+		CredentialId string `json:"credential_id" form:"credential_id" validate:"required,max=64"`
+		RefreshToken string `json:"refresh_token" form:"refresh_token" validate:"required,max=500"`
 	}
 
 	RefreshTokenRes struct {
-		Id string `json:_id`
+		Id 	string `json:"_id"`
 		UserId string `json:"user_id"`
 		RoleCode []int `json:"role_code"`
 		RefreshToken string `json:"refresh_token"`
@@ -32,7 +32,7 @@ type (
 
 	InsertUserRole struct {
 		UserId string `json:"user_id" validate:"required"`
-		Role  []int `json:"role_code" validate:"required"`
+		RoleCode []int  `json:"role_id" validate:"required"`
 	}
 
 	ProfileIntercepter struct {
