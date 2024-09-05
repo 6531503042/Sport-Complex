1c61ccb9021039f62ecac09e5afb1a588f29b5c7
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-05 23:20:08 +0700

    Update grpc.go

--- backend/pkg/grpc/grpc.go
+++ backend/pkg/grpc/grpc.go
@@ -39,26 +39,26 @@ func (g *grpcClientFactory) Auth() authPb.AuthGrpcServiceClient {
 }
 
 func NewGrpcClient(host string) (GrpcClientFactoryHandler, error) {
-    opts := make([]grpc.DialOption, 0)
+	opts := make([]grpc.DialOption, 0)
 
-    opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-    ClientConn, err := grpc.NewClient(host, opts...)
-    if err != nil {
-        log.Printf("Error: Grpc Client connection failed %s", err.Error())
-        return nil, errors.New("error: grpc client connection failed")
-    }
+	clientConn, err := grpc.Dial(host, opts...)
+	if err != nil {
+		log.Printf("Error: Grpc client connection failed: %s", err.Error())
+		return nil, errors.New("error: grpc client connection failed")
+	}
 
-    return &grpcClientFactory{
-        client: ClientConn,
-    }, nil
+	return &grpcClientFactory{
+		client: clientConn,
+	}, nil
 }
 
 func NewGrpcServer(cfg *config.Jwt, host string) (*grpc.Server, net.Listener) {
 	opts := make([]grpc.ServerOption, 0)
 
 	grpcAuth := &grpcAuth{
-		secretKey: cfg.AccessSecretKey,
+		secretKey: cfg.ApiSecretKey,
 	}
 
 	opts = append(opts, grpc.UnaryInterceptor(grpcAuth.unaryAuthorization))
@@ -67,37 +67,36 @@ func NewGrpcServer(cfg *config.Jwt, host string) (*grpc.Server, net.Listener) {
 
 	lis, err := net.Listen("tcp", host)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Error: Failed to listen: %v", err)
 	}
 
 	return grpcServer, lis
 }
 
-func (g *grpcAuth) unaryAuthorization (ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+func (g *grpcAuth) unaryAuthorization(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
 		log.Printf("Error: Metadata not found")
-		return nil, errors.New("metadata not found")
+		return nil, errors.New("error: metadata not found")
 	}
 
 	authHeader, ok := md["auth"]
 	if !ok {
-		log.Printf("Error: Auth header not found")
-		return nil, errors.New("auth header not found")
+		log.Printf("Error: Metadata not found")
+		return nil, errors.New("error: metadata not found")
 	}
 
 	if len(authHeader) == 0 {
-		log.Printf("Error: Auth header not found")
-		return nil, errors.New("auth header not found")
+		log.Printf("Error: Metadata not found")
+		return nil, errors.New("error: metadata not found")
 	}
 
 	claims, err := jwt.ParseToken(g.secretKey, string(authHeader[0]))
 	if err != nil {
-		log.Printf("Error: ParseToken: %s", err.Error())
-		return nil, errors.New("parse token failed")
+		log.Printf("Error: Parse token failed: %s", err.Error())
+		return nil, errors.New("error: token is invalid")
 	}
 	log.Printf("claims: %v", claims)
+
 	return handler(ctx, req)
 }
-
-
