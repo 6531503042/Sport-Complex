49ff3bf9ec5ad56a857dcd8c780df7d814e5805e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-05 23:03:27 +0700

    Update middlewareRepository.go

--- backend/modules/middleware/middlewareRepository/middlewareRepository.go
+++ backend/modules/middleware/middlewareRepository/middlewareRepository.go
@@ -25,7 +25,7 @@ func NewMiddlewareRepository() MiddlewareRepositoryService {
 	return &middlewareRepository{}
 }
 
-func (r *middlewareRepository) AccessTokenSearch (pctx context.Context, grpcUrl, accessToken string) error {
+func (r *middlewareRepository) AccessTokenSearch(pctx context.Context, grpcUrl, accessToken string) error {
 	ctx, cancel := context.WithTimeout(pctx, 30*time.Second)
 	defer cancel()
 
@@ -40,18 +40,18 @@ func (r *middlewareRepository) AccessTokenSearch (pctx context.Context, grpcUrl,
 		AccessToken: accessToken,
 	})
 	if err != nil {
-		log.Printf("Error: AccessTokenSearch failed: %s", err.Error())
-		return errors.New("error: AccessTokenSearch failed")
+		log.Printf("Error: CredentialSearch failed: %s", err.Error())
+		return errors.New("error: email or password is incorrect")
 	}
 
 	if result == nil {
-		log.Printf("Error: AccessTokenSearch result is nil")
-		return errors.New("error: AccessTokenSearch result is nil")
+		log.Printf("Error: access token is invalid")
+		return errors.New("error: access token is invalid")
 	}
 
-	if (!result.IsValid) {
-		log.Printf("Error: AccessTokenSearch result is invalid")
-		return errors.New("error: AccessTokenSearch result is invalid")
+	if !result.IsValid {
+		log.Printf("Error: access token is invalid")
+		return errors.New("error: access token is invalid")
 	}
 
 	return nil
@@ -64,9 +64,9 @@ func (r *middlewareRepository) RolesCount(pctx context.Context, grpcUrl string) 
 	conn, err := grpc.NewGrpcClient(grpcUrl)
 	if err != nil {
 		log.Printf("Error: gRPC connection failed: %s", err.Error())
-		return 0, errors.New("error: gRPC connection failed")
+		return -1, errors.New("error: gRPC connection failed")
 	}
-	
+
 	jwt.SetApiKeyInContext(&ctx)
 	result, err := conn.Auth().RolesCount(ctx, &authPb.RolesCountReq{})
 	if err != nil {
