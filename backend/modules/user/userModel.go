298607a7e37cea1f161a9ac83fb1a15ef90a64bc
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-05 23:03:37 +0700

    Update userModel.go

--- backend/modules/user/userModel.go
+++ backend/modules/user/userModel.go
@@ -2,21 +2,24 @@ package user
 
 import "time"
 
-type UserProfile struct {
-	Id        string    `json:"id"`
-	Email     string    `json:"email"`
-	Name      string    `json:"name"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
+type (
+	UserProfile struct {
+		Id        string    `json:"id"`
+		Email     string    `json:"email"`
+		Name      string    `json:"name"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
 
-type UserClaims struct {
-	Id       string `json:"id"`
-	RoleCode []int  `json:"role_code"`
-}
+	UserClaims struct {
+		Id       string `json:"id"`
+		RoleCode []int  `json:"role_code"`
+	}
 
-type CreateUserReq struct {
-	Email    string `json:"email" form:"email" validate:"required,email,max=255"`
-	Password string `json:"password" form:"password" validate:"required,max=32"`
-	Name     string `json:"name" form:"name" validate:"required,max=32"`
-}
+	CreateUserReq struct {
+		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
+		Password string `json:"password" form:"password" validate:"required,max=32"`
+		Name     string `json:"name" form:"name" validate:"required,max=32"`
+	}
+	
+)
