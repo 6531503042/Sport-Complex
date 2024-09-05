558e2f6a623cd29503a4bf5be6acd0643715a460
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-05 17:56:13 +0700

    a

---
+++ front-1/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import HomePage from './pages/homepage/page'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <HomePage/>
+    </main>
+  );
+}
---
+++ front-1/src/app/pages/batminton/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../../components/navbar/navbar'
+
+const page = () => {
+  return (
+    <div>
+        <NavBar activePage="basketball"/>
+        <div className=''>
+          basketball
+        </div>
+    </div>
+  )
+}
+
+export default page
