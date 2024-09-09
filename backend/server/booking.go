ff91fb850d05f4afdb16c636e534d2b2c7815def
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-09 23:24:34 +0700

    Server: Enchance booking usecase route.

--- backend/server/booking.go
+++ backend/server/booking.go
@@ -10,11 +10,12 @@ func (s *server) bookingService() {
 	// Initialize repository, usecase, and handlers
 	repo := repository.NewBookingRepository(s.db)
 	bookingUsecase := usecase.NewBookingUsecase(repo)
-	httpHander := handler.NewBookingHttpHandlerService(s.cfg, bookingUsecase)
+	httpHandler := handler.NewBookingHttpHandler(s.cfg, bookingUsecase)  // Call the correct constructor function
 
+	// Setup the booking routes
 	booking := s.app.Group("/booking_v1")
-	booking.POST("/bookings/create", httpHander.InsertBooking)
-	booking.GET("/bookings/:booking_id", httpHander.FindBooking)
-	booking.GET("/bookings/user/:user_id", httpHander.FindOneUserBooking)
-	booking.PUT("/bookings/:id", httpHander.UpdateBooking)
+	booking.POST("/bookings", httpHandler.InsertBooking)
+	booking.GET("/bookings/:booking_id", httpHandler.FindBooking)
+	booking.GET("/bookings/user/:user_id", httpHandler.FindOneUserBooking)
+	booking.PUT("/bookings/:id", httpHandler.UpdateBooking)
 }
