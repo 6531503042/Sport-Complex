34344a1d9729f391b543096a3ae9bd328571075e
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-10 13:27:49 +0700

    Create bookingEntity.go

---
+++ backend/modules/booking/bookingEntity.go
@@ -0,0 +1,27 @@
+package booking
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Booking struct {
+		Id        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+		UserId    string             `bson:"user_id" json:"user_id"`
+		SlotId    string             `bson:"slot_id" json:"slot_id"`
+		Status    int                `bson:"status" json:"status"`
+		CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+		UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	}
+
+	Slot struct {
+		Id        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+		StartTime time.Time          `bson:"start_time" json:"start_time"`
+		EndTime   time.Time          `bson:"end_time" json:"end_time"`
+		Status    int                `bson:"status" json:"status"`
+		CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+		UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	}
+)
