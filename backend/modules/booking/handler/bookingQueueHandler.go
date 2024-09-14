17ed89412d8e3e5a780377be0b2f9f72dd1be311
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-14 22:45:12 +0700

    Update bookingQueueHandler.go

--- backend/modules/booking/handler/bookingQueueHandler.go
+++ backend/modules/booking/handler/bookingQueueHandler.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
+	"context"
+	"log"
 	"main/config"
 	"main/modules/booking/usecase"
+	"main/pkg/queue"
+
+	"github.com/IBM/sarama"
 )
 
 type (
@@ -20,9 +25,25 @@ func NewBookingQueueHandler(cfg *config.Config, bookingUsecase usecase.BookingUs
 	return &bookingQueueHandler{cfg: cfg, bookingUsecase: bookingUsecase}
 }
 
-// func (h *bookingQueueHandler) BookingConsumer(pctx context.Context) (sarama.PartitionConsumer, error) {
-// 	worker, err := queue.ConnectConsumer([]string{h.cfg.Kafka.Url}, h.cfg.Kafka.ApiKey, h.cfg.Kafka.Secret)
-// 	if err != nil {
-// 		log.Printf()
-// 	}
-// }
+func (h * bookingQueueHandler) BookingConsumer (pctx context.Context) (sarama.PartitionConsumer, error) {
+	worker, err := queue.ConnectConsumer([]string{h.cfg.Kafka.Url}, h.cfg.Kafka.ApiKey, h.cfg.Kafka.Secret)
+	if err != nil {
+		return nil, err
+	}
+
+	offset, err := h.bookingUsecase.GetOffSet(pctx)
+	if err != nil {
+		return nil, err
+	}
+	consumer, err := worker.ConsumePartition("booking", 0, offset)
+	if err != nil {
+		log.Println("Trying to set offset as 0")
+		consumer, err = worker.ConsumePartition("booking", 0, 0)
+		if err != nil {
+			log.Println("Error: BookingConsumer failed: ", err.Error())
+			return nil, err
+		}
+	}
+
+	return consumer, nil
+}
