2c89cd577077c878cde6c9b85f277c87165bcd9f
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-15 21:11:46 +0700

    update banner change style to carousel style

---
+++ frontend/src/app/components/banner/banner_main.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import Banner1Img from "../../assets/dark_bg.jpg";
+import Banner2Img from "../../assets/banner_2.jpg";
+import Banner3Img from "../../assets/banner_3.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+type BannerProps = {
+  onLeftClick: () => void;
+  onRightClick: () => void;
+};
+
+const BannerCarousel: React.FC = () => {
+  const banners = [
+    {
+      image: Banner1Img,
+      title: "Welcome to Sport Complex",
+      description: "Reserve your spot and never miss out! Easily schedule your favorite sports activities with just a few clicks.",
+      link: "/pages/registration",
+    },
+    {
+      image: Banner2Img,
+      title: "Stay Active, Stay Healthy",
+      description: "Discover a variety of sports and fitness programs tailored to your needs.",
+      link: "/pages/programs",
+    },
+    {
+      image: Banner3Img,
+      title: "Join a Community",
+      description: "Meet like-minded individuals and engage in exciting activities.",
+      link: "/pages/community",
+    },
+  ];
+
+  const [currentBanner, setCurrentBanner] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentBanner((prev) => (prev === banners.length - 1 ? 0 : prev + 1));
+    }, 5000); 
+
+    return () => clearInterval(interval); 
+  }, [banners.length]);
+
+  const handleLeftClick = () => {
+    setCurrentBanner((prev) => (prev === 0 ? banners.length - 1 : prev - 1));
+  };
+
+  const handleRightClick = () => {
+    setCurrentBanner((prev) => (prev === banners.length - 1 ? 0 : prev + 1));
+  };
+
+  return (
+    <div className="flex items-center h-[500px] text-white bg-cover bg-center px-10"
+      style={{
+        backgroundImage: `url(${banners[currentBanner].image.src})`,
+      }}
+    >
+      <div className="flex flex-row items-center w-screen justify-between">
+        <button
+          className="flex items-center justify-center w-12 h-12 rounded-full cursor-pointer text-8xl text-white border-white border-2 hover:border-yellow-400 hover:text-yellow-400"
+          onClick={handleLeftClick}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} style={{ fontSize: "1rem" }} />
+        </button>
+        <div className="flex flex-col h-auto w-1/2 text-center items-center">
+          <div className="flex flex-col p-4 items-center">
+            <p className="text-6xl font-bold">{banners[currentBanner].title}</p>
+            <span className="mt-5 w-2/3 text-lg">
+              {banners[currentBanner].description}
+            </span>
+          </div>
+          <div className="mt-3 p-3 bg-transparent w-fit border-2 border-stone-400 rounded-md text-white text-xs font-bold">
+            <a href={banners[currentBanner].link}>
+              <button type="button">Learn More</button>
+            </a>
+          </div>
+        </div>
+        <button
+          className="flex items-center justify-center w-12 h-12 rounded-full cursor-pointer text-white border-white border-2 hover:border-yellow-400 hover:text-yellow-400"
+          onClick={handleRightClick}
+        >
+          <FontAwesomeIcon icon={faChevronRight} style={{ fontSize: "1rem" }} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BannerCarousel;
--- frontend/src/app/pages/homepage/page.tsx
+++ frontend/src/app/pages/homepage/page.tsx
@@ -2,9 +2,7 @@
 
 import React, { useState } from "react";
 import NavBar from "../../components/navbar/navbar";
-import Banner1 from "../../components/banner/banner1";
-import Banner2 from "../../components/banner/banner2";
-import Banner3 from "../../components/banner/banner3";
+import BannerMain from "../../components/banner/banner_main"
 import "../../css/banner.css";
 import {
   faCircleExclamation,
@@ -14,45 +12,11 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const HomePage: React.FC = () => {
-  const [currentBanner, setCurrentBanner] = useState<1 | 2 | 3>(1);
-
-  const handleLeftClick = () => {
-    setCurrentBanner((prev) => {
-      if (prev === 1) return 3 as 1 | 2 | 3;
-      return (prev - 1) as 1 | 2 | 3;
-    });
-  };
-
-  const handleRightClick = () => {
-    setCurrentBanner((prev) => {
-      if (prev === 3) return 1 as 1 | 2 | 3;
-      return (prev + 1) as 1 | 2 | 3;
-    });
-  };
 
   return (
-    <div>
+    <div className="">
       <NavBar />
-      <div className="banner-container">
-        {currentBanner === 1 && (
-          <Banner1
-            onLeftClick={handleLeftClick}
-            onRightClick={handleRightClick}
-          />
-        )}
-        {currentBanner === 2 && (
-          <Banner2
-            onLeftClick={handleLeftClick}
-            onRightClick={handleRightClick}
-          />
-        )}
-        {currentBanner === 3 && (
-          <Banner3
-            onLeftClick={handleLeftClick}
-            onRightClick={handleRightClick}
-          />
-        )}
-      </div>
+      <BannerMain/>
       <div className=" gap-10 mt-10 mx-24 flex flex-row">
         <div className="flex flex-none w-2/3 flex-col">
           <h1 className="font-bold text-3xl">News ! ! ! !</h1>
