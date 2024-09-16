412e23770ae11c4d6689bdd3fa690caaedce3fa1
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-16 22:42:39 +0700

    Facility: Added entity.

---
+++ backend/modules/facility/facilityEntity.go
@@ -0,0 +1,19 @@
+package facility
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type (
+	Facilitiy struct {
+		ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+		Name        string             `bson:"name" json:"name"`
+		PriceInsider float64           `bson:"price_insider" json:"price_insider"`
+		PriceOutsider float64          `bson:"price_outsider" json:"price_outsider"`
+		Description string             `bson:"description" json:"description"`
+		CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
+		UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
+	}
+)
