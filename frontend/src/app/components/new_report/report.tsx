6be4c7cca22290bfc68dc06c34fca78a84ffa3dc
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-16 22:18:09 +0700

    update report

---
+++ frontend/src/app/components/new_report/report.tsx
@@ -0,0 +1,157 @@
+import React from 'react'
+import {
+    faCircleExclamation,
+    faClock,
+    faClockFour,
+  } from "@fortawesome/free-solid-svg-icons";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const report = () => {
+  return (
+    <div>
+        <div className=" gap-10 mt-10 mx-24 flex flex-row">
+        <div className="flex flex-none w-2/3 flex-col">
+          <h1 className="font-bold text-3xl">News ! ! ! !</h1>
+          <hr className="mt-3 border border-neutral-800" />
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-l-red-700 hover:border-y-0 hover:border-l-2 hover:bg-gray-50">
+            <div className="flex flex-col ps-5">
+              <div className="flex flew-row justify-between">
+                <p className="pt-2 text-sm text-gray-500">06 sep 2024</p>
+                <p className="bg-red-600 p-1 text-sm text-center rounded-b-lg text-white">
+                  <FontAwesomeIcon
+                    className="text-white me-1"
+                    icon={faCircleExclamation}
+                  />
+                  Newest
+                </p>
+              </div>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+                aliquam tempore. Rerum unde perspiciatis libero reiciendis ipsa
+                a debitis neque?
+              </span>
+              <div className="invisible">space</div>
+            </div>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-l-red-700 hover:border-y-0 hover:border-l-2 hover:bg-gray-50">
+            <div className="flex flex-col ps-5">
+              <div className="flex flew-row justify-between">
+                <p className="pt-2 text-sm text-gray-500">06 sep 2024</p>
+                <p className="bg-red-600 p-1 text-sm text-center rounded-b-lg text-white">
+                  <FontAwesomeIcon
+                    className="text-white me-1"
+                    icon={faCircleExclamation}
+                  />
+                  Newest
+                </p>
+              </div>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+                aliquam tempore. Rerum unde perspiciatis libero reiciendis ipsa
+                a debitis neque?
+              </span>
+              <div className="invisible">space</div>
+            </div>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-l-red-700 hover:border-y-0 hover:border-l-2 hover:bg-gray-50">
+            <div className="flex flex-col ps-5">
+              <div className="flex flew-row justify-between">
+                <p className="pt-2 text-sm text-gray-500">06 sep 2024</p>
+                <p className="bg-red-600 p-1 text-sm text-center rounded-b-lg text-white">
+                  <FontAwesomeIcon
+                    className="text-white me-1"
+                    icon={faCircleExclamation}
+                  />
+                  Newest
+                </p>
+              </div>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+                aliquam tempore. Rerum unde perspiciatis libero reiciendis ipsa
+                a debitis neque?
+              </span>
+              <div className="invisible">space</div>
+            </div>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-l-red-700 hover:border-y-0 hover:border-l-2 hover:bg-gray-50">
+            <div className="flex flex-col ps-5">
+              <div className="flex flew-row justify-between">
+                <p className="pt-2 text-sm text-gray-500">06 sep 2024</p>
+                <p className="bg-red-600 p-1 text-sm text-center rounded-b-lg text-white">
+                  <FontAwesomeIcon
+                    className="text-white me-1"
+                    icon={faCircleExclamation}
+                  />
+                  Newest
+                </p>
+              </div>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+                aliquam tempore. Rerum unde perspiciatis libero reiciendis ipsa
+                a debitis neque?
+              </span>
+              <div className="invisible">space</div>
+            </div>
+            <hr />
+          </div>
+        </div>
+        <div className="flex flex-none w-1/3 flex-col border border-b-0">
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+          <div className="cursor-pointer border-l-2 border-l-transparent hover:border-red-500 hover:bg-gray-50">
+            <p className="p-5 text-2xl text-gray-700">
+              <FontAwesomeIcon className="text-red-500 me-2" icon={faClockFour} />
+              Lorem, ipsum dolor.
+            </p>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default report
