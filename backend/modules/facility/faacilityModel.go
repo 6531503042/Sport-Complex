2a9a778279c58ef565564f091156929037d40be3
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-16 22:42:44 +0700

    Facility: Added model.

---
+++ backend/modules/facility/faacilityModel.go
@@ -0,0 +1,29 @@
+package facility
+
+import "time"
+
+type (
+	CreateFaciliityRequest struct {
+		Name          string  `json:"name" validate:"required"`
+		PriceInsider  float64 `json:"price_insider" validate:"required"`
+		PriceOutsider float64 `json:"price_outsider" validate:"required"`
+		Description   string  `json:"description" validate:"required"`
+	}
+
+	UpdateFaciltyRequest struct {
+		Name          string  `json:"name,omitempty"`
+		PriceInsider  float64 `json:"price_insider,omitempty"`
+		PriceOutsider float64 `json:"price_outsider,omitempty"`
+		Description   string  `json:"description,omitempty"`
+	}
+
+	FacilitiyResponse struct {
+		ID            string    `json:"id"`
+		Name          string    `json:"name"`
+		PriceInsider  float64   `json:"price_insider"`
+		PriceOutsider float64   `json:"price_outsider"`
+		Description   string    `json:"description"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+	}
+)
