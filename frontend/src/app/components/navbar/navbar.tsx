4ae206895bd7a5ea6011d2bc4d99cf7417eed4f7
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-17 23:36:31 +0700

    update search bar

--- frontend/src/app/components/navbar/navbar.tsx
+++ frontend/src/app/components/navbar/navbar.tsx
@@ -13,7 +13,7 @@ import {
   faSwimmer,
   faWallet,
 } from "@fortawesome/free-solid-svg-icons";
-import { faDumbbell } from "@fortawesome/free-solid-svg-icons/faDumbbell";
+import SearhBar from "../../components/search_bar/search_bar";
 
 type NavBarProps = {
   activePage?: string;
@@ -50,36 +50,39 @@ const navbar: React.FC<NavBarProps> = ({ activePage }) => {
   return (
     <div className={`${getBackgroundColor()} justify-center flex flex-col`}>
       <header>
-      <div className="NavBar_container flex flex-row items-center justify-between bg-white px-8 py-5">
-        <Link
-          href="/"
-          className="inline-flex flex-row items-center flex-none gap-3.5"
-        >
-          <img src={Logo.src} alt="Logo" className="w-7" />
-          <span className="flex flex-col border-l-2 w-max">
-            <div className="ms-1">
-              <span className="ms-1 inline-flex flex-row font-semibold text-xl">
-                <p className="text-black ">SPORT.</p>
-                <p className="text-gray-500">MFU</p>
-              </span>
-              <hr />
-              <span className="text-gray-500 ms-1 font-medium text-sm">
-                SPORT COMPLEX
-              </span>
-            </div>
-          </span>
-        </Link>
-        <div className="login_and_sidebar flex-none flex justify-between ms-5 me-2 gap-10">
-          <aside className="login_button border-b-4 border-transparent">
-            <nav className="inline-flex">
-              <div className="text-black hover:text-gray-400 cursor-pointer">
-                <Link href="/pages/registration">login</Link>
+        <div className="NavBar_container flex flex-row items-center justify-between bg-white px-8 py-5">
+          <Link
+            href="/"
+            className="inline-flex flex-row items-center flex-none gap-3.5 w-1/5"
+          >
+            <img src={Logo.src} alt="Logo" className="w-7" />
+            <span className="flex flex-col border-l-2 w-max">
+              <div className="ms-1">
+                <span className="ms-1 inline-flex flex-row font-semibold text-xl">
+                  <p className="text-black ">SPORT.</p>
+                  <p className="text-gray-500">MFU</p>
+                </span>
+                <hr />
+                <span className="text-gray-500 ms-1 font-medium text-sm">
+                  SPORT COMPLEX
+                </span>
               </div>
-            </nav>
-          </aside>
-          <SideBar />
+            </span>
+          </Link>
+          <div className="flex-none w-3/5 justify-end flex">
+            <SearhBar />
+          </div>
+          <div className="login_and_sidebar flex-none w-1/12 flex justify-end ms-5 me-2 gap-20">
+            <aside className="login_button border-b-4 border-transparent">
+              <nav className="inline-flex">
+                <div className="text-black hover:text-gray-400 cursor-pointer">
+                  <Link href="/pages/registration">login</Link>
+                </div>
+              </nav>
+            </aside>
+            <SideBar />
+          </div>
         </div>
-      </div>
       </header>
       <ul className="NavBar_res inline-flex flex-row pr-10 gap-16 justify-center font-semibold pt-4 text-sm">
         <li className={getActiveClass("swimming")}>
--- frontend/src/app/components/search_bar/search_bar.tsx
+++ frontend/src/app/components/search_bar/search_bar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const search_bar = () => {
   return (
-    <div>search_bar</div>
-  )
-}
+    <div className="border-gray-400 border py-2">
+      <input type="text" placeholder="Search . . ." className="text-xs border-none"/>
+      <button className="px-4">
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
+    </div>
+  );
+};
 
-export default search_bar
+export default search_bar;
