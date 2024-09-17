4e35089d4e1b419212f493569932eb5933a72926
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-17 15:41:49 +0700

    Facility: Added FacilityBson struct

--- backend/modules/facility/facilityEntity.go
+++ backend/modules/facility/facilityEntity.go
@@ -16,4 +16,14 @@ type (
 		CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
 		UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
 	}
+
+	FacilityBson struct {
+		ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+		Name        string             `bson:"name" json:"name"`
+		PriceInsider float64           `bson:"price_insider" json:"price_insider"`
+		PriceOutsider float64          `bson:"price_outsider" json:"price_outsider"`
+		Description string             `bson:"description" json:"description"`
+		CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
+		UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
+	}
 )
