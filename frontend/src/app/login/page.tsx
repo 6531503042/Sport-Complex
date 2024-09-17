d35c5d3c17b4f6ffd3eac9b1b1547d003f62eb42
Softie_tie <6531503005@lamduan.mfu.ac.th>
2024-09-17 22:14:13 +0700

    update layout login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -9,34 +9,38 @@ const LoginPage = () => {
       <div className={styles.left}>
         <Image className={styles.logo} src="/assets/logo-mfu-v2.png" alt="Logo" width={75} height={75} />
         <h1 className={styles.header}>Welcome Back</h1>
-        <p className={styles.uderheader}>Welcome back to MFU Sport complex.</p>
+        <p className={styles.underheader}>Welcome back to MFU Sport complex.</p>
         <form className={styles.form}>
-          <Input
-            fullWidth
-            isClearable
-            label="Email"
-            placeholder="Enter your email"
-            type="email"
-            className={styles.input} 
-          />
-          <Input
-            fullWidth
-            isClearable
-            label="Password"
-            placeholder="********"
-            type="password"
-            className={styles.input} 
-          />
+          <div className={styles.input}>
+            <Input
+              fullWidth
+              isClearable
+              label="Email"
+              placeholder="Enter your lamduan email"
+              type="email"
+            />
+          </div>
+          <div className={styles.input}>
+            <Input
+              fullWidth
+              isClearable
+              label="Password"
+              placeholder="********"
+              type="password"
+            />
+          </div>
           <div className={styles.checkboxContainer}>
-            <Checkbox className={styles.checkbox} />
-            <span>Remember me</span>
+            <div className={styles.checkboxWrapper}>
+              <Checkbox className={styles.checkbox} />
+              <span>Remember me</span>
+            </div>
             <Link href="#">Forgot password</Link>
           </div>
           <Button type="submit" className={styles.button} color="primary">
             Sign in
           </Button>
           <p className={styles.textCenter}>
-            Are you an outsider? <Link href="#">Sign up for free!</Link>
+            Are you outsider? <Link href="#">Can Sign up for free!</Link>
           </p>
         </form>
       </div>
