43f6c0f3b732176e9b4449539f8bc06dbc4c4e41
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-18 14:53:09 +0700

    Usecase: Added Func CRUD Repo.

--- backend/modules/facility/usecase/facilityUsecase.go
+++ backend/modules/facility/usecase/facilityUsecase.go
@@ -7,11 +7,16 @@ import (
 	"main/modules/facility"
 	"main/modules/facility/repository"
 	"main/pkg/utils"
+	"time"
 )
 
 type (
 	FacilityUsecaseService interface {
-
+		CreateFacility (pctx context.Context, req *facility.CreateFaciliityRequest) (facility.FacilityBson, error)
+		FindOneFacility (pctx  context.Context, facilityId string) (*facility.FacilityBson, error)
+		FindManyFacility (pctx context.Context) ([]facility.FacilityBson, error)
+		UpdateOneFacility (pctx context.Context, facilityId string, updateFields map[string]interface{}) error
+		DeleteOneFacility (pctx context.Context, facilityId string) error
 	}
 
 	facilityUsecase struct {
@@ -39,10 +44,77 @@ func (u *facilityUsecase) CreateFacility (pctx context.Context, req *facility.Cr
 		CreatedAt: utils.LocalTime(),
 		UpdatedAt: utils.LocalTime(),
 	})
-	if err != nil {
-		return facility.FacilityBson{}, fmt.Errorf("error: insert facility failed: %w", err)
-	}
+	// Find the newly inserted facility
+    facilityBson, err := u.facilityRepository.FindOneFacility(pctx, facilityId.Hex())
+    if err != nil {
+        return facility.FacilityBson{}, fmt.Errorf("error: find facility failed: %w", err)
+    }
+    if facilityBson == nil {
+        return facility.FacilityBson{}, errors.New("error: facility not found")
+    }
 
-	return *u.facilityRepository.FindOneFacility(pctx, facilityId.Hex())
+    return *facilityBson, nil
 }
 
+
+func (u *facilityUsecase) FindOneFacility (pctx  context.Context, facilityId string) (*facility.FacilityBson, error) {
+	result, err := u.facilityRepository.FindOneFacility(pctx, facilityId)
+	if err != nil {
+		return nil, err
+	}
+
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+        return nil, fmt.Errorf("error: unable to load time location: %w", err)
+    }
+
+	return &facility.FacilityBson{
+        Id:            result.Id, // No need to convert ObjectID to Hex manually
+        Name:          result.Name,
+        PriceInsider:  result.PriceInsider,
+        PriceOutsider: result.PriceOutsider,
+        Description:   result.Description,
+        CreatedAt:     result.CreatedAt.In(loc),
+        UpdatedAt:     result.UpdatedAt.In(loc),
+    }, nil
+}
+
+func (u *facilityUsecase) FindManyFacility (pctx context.Context) ([]facility.FacilityBson, error) {
+	results, err := u.facilityRepository.FindManyFacility(pctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var facilityProfile []facility.FacilityBson
+	for _, result := range results {
+		facilityProfile = append(facilityProfile, facility.FacilityBson{
+			Id:            result.Id, // No need to convert ObjectID to Hex manually
+			Name:          result.Name,
+			PriceInsider:  result.PriceInsider,
+			PriceOutsider: result.PriceOutsider,
+			Description:   result.Description,
+			CreatedAt:     result.CreatedAt,
+			UpdatedAt:     result.UpdatedAt,
+		})
+	}
+
+	return facilityProfile, nil
+}
+
+func (u *facilityUsecase) UpdateOneFacility (pctx context.Context, facilityId string, updateFields map[string]interface{}) error {
+	if _, err := u.facilityRepository.FindOneFacility(pctx, facilityId); err != nil {
+		return err
+	}
+
+	updateFields["updated_at"] = utils.LocalTime().Format(time.RFC3339)
+	return u.facilityRepository.UpdateOneFacility(pctx, facilityId, updateFields)
+}
+
+func (u *facilityUsecase) DeleteOneFacility (pctx context.Context, facilityId string) error {
+	_, err := u.facilityRepository.FindOneFacility(pctx, facilityId)
+	if err != nil {
+		return err
+	}
+
+	return u.facilityRepository.DeleteOneFacility(pctx,facilityId)
+}
