5172b28af343b73c23694fff1364f32360a9fd2a
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-19 19:42:03 +0700

    Update search_bar.tsx

--- frontend/src/app/components/search_bar/search_bar.tsx
+++ frontend/src/app/components/search_bar/search_bar.tsx
@@ -12,9 +12,9 @@ const search_bar = () => {
   };
 
   return (
-    <div className="flex justify-end w-full items-center">
+    <div className="flex w-full items-center">
       <div
-        className={`flex items-center transition-all duration-500 ease-in-out ${
+        className={`flex justify-end items-center transition-all duration-500 ease-in-out ${
           isExpanded ? "w-64 border px-2 py-2" : "w-10"
         } border-gray-400 rounded-full w-full`}
       >
@@ -28,7 +28,7 @@ const search_bar = () => {
         <button
           onClick={toggleSearch}
           className={`flex justify-center items-center transition-all duration-300 mx-1 ${
-            isOpen ? "border px-2 py-2 rounded-full border-gray-400 hover:border-orange-700 hover:text-orange-700 text-gray-500 " : "text-gray-500 hover:bg-orange-700 hover:text-white rounded-full px-2 py-2"
+            isOpen ? "border border-transparent px-2 py-2 rounded-full  hover:bg-orange-700 hover:text-white text-gray-500 " : "text-gray-500 hover:bg-orange-700 hover:text-white rounded-full px-2 py-2"
           }`}
         >
           <FontAwesomeIcon icon={faSearch} className="" />
