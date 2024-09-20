8eba24a6982666a301aa031a477cd2417c699da2
franz1us <franzius.won@gmail.com>
2024-09-20 16:01:12 +0700

    add responsive for mobile
    add responsive for mobile

--- frontend/src/app/(Booking)/swimming-booking/page.tsx
+++ frontend/src/app/(Booking)/swimming-booking/page.tsx
@@ -8,13 +8,13 @@ interface Data {
   member: string;
 }
 
-function Football() {
+function Swimming_Booking() {
   const data: Data[] = [
-    { time: "10.00 - 11.00", isAvailable: true, member: "0/30" },
-    { time: "11.00 - 12.00", isAvailable: false, member: "30/30" },
-    { time: "12.00 - 13.00", isAvailable: true, member: "0/30" },
-    { time: "13.00 - 14.00", isAvailable: false, member: "30/30" },
-    { time: "14.00 - 15.00", isAvailable: true, member: "0/30" },
+    { time: "10.00 - 12.00", isAvailable: true, member: "0/30" },
+    { time: "12.00 - 14.00", isAvailable: false, member: "30/30" },
+    { time: "14.00 - 16.00", isAvailable: true, member: "0/30" },
+    { time: "16.00 - 18.00", isAvailable: false, member: "30/30" },
+    { time: "18.00 - 20.00", isAvailable: true, member: "0/30" },
   ];
 
   const [selectedCard, setSelectedCard] = useState<number | null>(null);
@@ -29,11 +29,16 @@ function Football() {
     phone: "",
   });
   const [isBookingSuccessful, setIsBookingSuccessful] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false); // New state for mobile view
 
   const handleCardClick = (index: number, isAvailable: boolean) => {
     if (isAvailable) {
       setSelectedCard(index === selectedCard ? null : index);
-      setErrors({ name: "", id: "", phone: "" }); 
+      setErrors({ name: "", id: "", phone: "" });
+
+      if (window.innerWidth < 640) { // Mobile screen size (sm breakpoint in Tailwind)
+        setIsMobileView(true); // Switch to form view in mobile
+      }
     }
   };
 
@@ -46,11 +51,10 @@ function Football() {
     };
     setErrors(newErrors);
 
-    const hasErrors = Object.values(newErrors).some(error => error !== "");
+    const hasErrors = Object.values(newErrors).some((error) => error !== "");
     if (!hasErrors) {
-      console.log("Form Booking successfully", formData);
+      console.log("Form submitted successfully", formData);
       setIsBookingSuccessful(true);
-
 
       setFormData({
         name: "",
@@ -58,7 +62,7 @@ function Football() {
         phone: "",
       });
       setSelectedCard(null);
-
+      setIsMobileView(false); // Reset view after booking
     }
   };
 
@@ -69,47 +73,24 @@ function Football() {
     });
   };
 
+  const handleBackToTimeSlots = () => {
+    setIsMobileView(false); // Go back to time slots in mobile
+  };
+
   return (
     <>
       <div className="flex flex-col items-center h-screen p-6">
         <div className="w-full max-w-[1189px] bg-[#FEFFFE] border-gray border rounded-3xl drop-shadow-2xl p-5">
-          <h1 className="text-4xl font-bold my-10 text-black text-center">Swimming Booking</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {data.map((item, index) => (
-              <div
-                key={index}
-                className={`bg-white border border-gray-200 rounded-lg p-4 shadow-md transition-transform duration-300 ease-in-out
-                  ${item.isAvailable ? "cursor-pointer" : "cursor-not-allowed"}
-                  ${selectedCard === index && item.isAvailable ? "bg-green-100 border-green-500" : ""}
-                  ${!item.isAvailable ? "bg-gray-200 text-gray-500" : "hover:bg-green-100"}`}
-                onClick={() => handleCardClick(index, item.isAvailable)}
-              >
-                <div className="text-lg font-semibold mb-2 flex justify-between">
-                  <div>{item.time}</div>
-                  <div
-                    className={`text-sm ${item.isAvailable ? "text-black" : "text-gray-500"}`}
-                  >
-                    {item.member}
-                  </div>
-                </div>
+          <h1 className="text-4xl font-bold my-10 text-black text-center">
+            Swimming Booking
+          </h1>
 
-                <div
-                  className={`text-sm ${item.isAvailable ? "text-green-500" : "text-gray-500"}`}
-                >
-                  {item.isAvailable ? "Available" : "Unavailable"}
-                </div>
-              </div>
-            ))}
-          </div>
-
-          <div
-            className={`transition-all duration-300 ease-in-out mt-6 p-4 bg-white border border-gray-200 rounded-lg shadow-md transform ${
-              selectedCard !== null ? "translate-y-0 opacity-100" : "translate-y-5 opacity-0"
-            }`}
-          >
-            {selectedCard !== null && data[selectedCard].isAvailable && (
-              <>
-                <h2 className="text-xl font-bold mb-4">Booking for {data[selectedCard].time}</h2>
+          {isMobileView ? ( // Mobile View of form
+            <div className="block sm:hidden">
+              <div className="bg-white border border-gray-200 rounded-lg p-4 shadow-md">
+                <h2 className="text-xl font-bold mb-4">
+                  Booking for {selectedCard !== null && data[selectedCard].time}
+                </h2>
                 <form onSubmit={handleSubmit}>
                   <label className="block mb-4">
                     <span className="block text-sm font-medium text-gray-700">Name</span>
@@ -119,23 +100,21 @@ function Football() {
                       value={formData.name}
                       onChange={handleChange}
                       placeholder="Enter your name"
-                      className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm transition-transform transform ${
-                        selectedCard !== null ? "translate-x-0 opacity-100" : "translate-x-10 opacity-0"
-                      }`}
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
                     />
                     {errors.name && <span className="text-red-500 text-sm">{errors.name}</span>}
                   </label>
                   <label className="block mb-4">
-                    <span className="block text-sm font-medium text-gray-700">Lecturer / Staff / Student ID</span>
+                    <span className="block text-sm font-medium text-gray-700">
+                      Lecturer / Staff / Student ID
+                    </span>
                     <input
                       type="text"
                       name="id"
                       value={formData.id}
                       onChange={handleChange}
                       placeholder="Enter your ID"
-                      className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm transition-transform transform ${
-                        selectedCard !== null ? "translate-x-0 opacity-100" : "translate-x-10 opacity-0"
-                      }`}
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
                     />
                     {errors.id && <span className="text-red-500 text-sm">{errors.id}</span>}
                   </label>
@@ -147,41 +126,146 @@ function Football() {
                       value={formData.phone}
                       onChange={handleChange}
                       placeholder="Enter your phone number"
-                      className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm transition-transform transform ${
-                        selectedCard !== null ? "translate-x-0 opacity-100" : "translate-x-10 opacity-0"
-                      }`}
+                      className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
                     />
                     {errors.phone && <span className="text-red-500 text-sm">{errors.phone}</span>}
                   </label>
                   <button
                     type="submit"
-                    className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-transform transform"
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
                   >
                     Booking
                   </button>
+                  <button
+                    type="button"
+                    className="bg-gray-500 text-white px-4 py-2 rounded-md ml-4 hover:bg-gray-600"
+                    onClick={handleBackToTimeSlots}
+                  >
+                    Back to Time Slots
+                  </button>
                 </form>
-              </>
-            )}
-          </div>
-        </div>
-
-        {isBookingSuccessful && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-              <h2 className="text-xl font-bold mb-2">Booking Successful!</h2>
-              <p className="text-green-500">You have successfully booked the slot.</p>
-              <button
-                className="mt-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-                onClick={() => setIsBookingSuccessful(false)}
-              >
-                Close
-              </button>
+              </div>
             </div>
-          </div>
-        )}
+          ) : (
+
+            // Normal screen view
+            <>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+                {data.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`bg-white border border-gray-200 rounded-lg p-4 shadow-md transition-transform duration-300 ease-in-out
+                      ${item.isAvailable ? "cursor-pointer" : "cursor-not-allowed"}
+                      ${
+                        selectedCard === index && item.isAvailable
+                          ? "bg-blue-100 border-blue-500"
+                          : ""
+                      }
+                      ${!item.isAvailable ? "bg-gray-200 text-gray-500" : "hover:bg-blue-100"}`}
+                    onClick={() => handleCardClick(index, item.isAvailable)}
+                  >
+                    <div className="text-lg font-semibold mb-2 flex justify-between">
+                      <div>{item.time}</div>
+                      <div
+                        className={`text-sm ${item.isAvailable ? "text-black" : "text-gray-500"}`}
+                      >
+                        {item.member}
+                      </div>
+                    </div>
+
+                    <div
+                      className={`text-sm ${
+                        item.isAvailable ? "text-blue-500" : "text-gray-500"
+                      }`}
+                    >
+                      {item.isAvailable ? "Available" : "Unavailable"}
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <div
+                className={`hidden sm:block transition-all duration-300 ease-in-out mt-6 p-4 bg-white border border-gray-200 rounded-lg shadow-md transform ${
+                  selectedCard !== null ? "translate-y-0 opacity-100" : "translate-y-5 opacity-0"
+                }`}
+              >
+                {selectedCard !== null && data[selectedCard].isAvailable && (
+                  <>
+                    <h2 className="text-xl font-bold mb-4">
+                      Booking for {data[selectedCard].time}
+                    </h2>
+                    <form onSubmit={handleSubmit}>
+                      <label className="block mb-4">
+                        <span className="block text-sm font-medium text-gray-700">Name</span>
+                        <input
+                          type="text"
+                          name="name"
+                          value={formData.name}
+                          onChange={handleChange}
+                          placeholder="Enter your name"
+                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                        />
+                        {errors.name && (
+                          <span className="text-red-500 text-sm">{errors.name}</span>
+                        )}
+                      </label>
+                      <label className="block mb-4">
+                        <span className="block text-sm font-medium text-gray-700">
+                          Lecturer / Staff / Student ID
+                        </span>
+                        <input
+                          type="text"
+                          name="id"
+                          value={formData.id}
+                          onChange={handleChange}
+                          placeholder="Enter your ID"
+                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                        />
+                        {errors.id && <span className="text-red-500 text-sm">{errors.id}</span>}
+                      </label>
+                      <label className="block mb-4">
+                        <span className="block text-sm font-medium text-gray-700">Phone Number</span>
+                        <input
+                          type="number"
+                          name="phone"
+                          value={formData.phone}
+                          onChange={handleChange}
+                          placeholder="Enter your phone number"
+                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md"
+                        />
+                        {errors.phone && <span className="text-red-500 text-sm">{errors.phone}</span>}
+                      </label>
+                      <button
+                        type="submit"
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                      >
+                        Booking
+                      </button>
+                    </form>
+                  </>
+                )}
+              </div>
+            </>
+          )}
+
+          {isBookingSuccessful && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+                <h2 className="text-xl font-bold mb-2">Booking Successful!</h2>
+                <p className="text-blue-500">You have successfully booked the slot.</p>
+                <button
+                  className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                  onClick={() => setIsBookingSuccessful(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
 }
 
-export default Football;
+export default Swimming_Booking;
