8f1948c031911444bf2b2c5145fa3469f3d4ff6b
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-21 22:09:36 +0700

    Facility: Added route for slot, and head route

--- backend/server/facility.go
+++ backend/server/facility.go
@@ -7,15 +7,18 @@ import (
 )
 
 func (s *server) facilityService() {
-
-	// TODO
 	repo := repository.NewFacilityRepository(s.db)
 	facilityUsecase := usecase.NewFacilityUsecase(repo)
-	httpHander := handler.NewFacilityHttpHandler(s.cfg, facilityUsecase)
+	httpHandler := handler.NewFacilityHttpHandler(s.cfg, facilityUsecase)
 
 	facility := s.app.Group("/facility_v1")
-	facility.GET("/facility/facilities", httpHander.FindManyFacility)
-	facility.GET("/facility/:facility_id", httpHander.FindOneFacility)
-	facility.POST("/facility/facility", httpHander.CreateFacility)
+	facility.GET("/facility/facilities", httpHandler.FindManyFacility)
+	facility.GET("/facility/:facility_id", httpHandler.FindOneFacility)
+	facility.POST("/facility/facility", httpHandler.CreateFacility)
 
+	// Slot Routes
+	facilitySlot := facility.Group("/:facilityName/slot_v1") // Corrected this line
+	facilitySlot.POST("/slots", httpHandler.InsertSlot)         // Create a new slot for a specific facility
+	facilitySlot.GET("/slots/:slot_id", httpHandler.FindOneSlot) // GET a slot by slot_id for a specific facility
+	facilitySlot.GET("/slots", httpHandler.FindAllSlots)        // GET all slots for a specific facility
 }
