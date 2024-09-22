add55950704aabd26b9837d016bb7b0b73de3a44
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-22 18:50:44 +0700

    add meterial icon in

--- front-end/src/app/(pages)/gym/page.tsx
+++ front-end/src/app/(pages)/gym/page.tsx
@@ -3,10 +3,15 @@ import NavBar from '../../components/navbar/navbar'
 
 const page = () => {
   return (
-    <div className=''>
+    <div className='bg-red-500'>
         <NavBar activePage="gym"/>
         <div className='w-full h-screen inline-flex flex-row'>
-          <div className='bg-red-500 flex-none w-2/3'>1</div>
+          <div className='bg-red-500 flex-none w-2/3'>
+          <div>
+      <h1>Material Icon Football</h1>
+      <i className="material-icons">sports_soccer</i>
+    </div>
+          </div>
           <div className='bg-black flex-none w-1/3'>2</div>
         </div>
     </div>
