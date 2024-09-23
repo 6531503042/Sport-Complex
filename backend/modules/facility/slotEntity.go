7a1cea11edaf53883ff5c8a2323b17370bad7ff4
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-23 21:12:25 +0700

    Update slotEntity.go

--- backend/modules/facility/slotEntity.go
+++ backend/modules/facility/slotEntity.go
@@ -20,16 +20,17 @@ type (
 	}
 
 	BadmintonCourt struct {
+		Id              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 		CourtNumber     int                `bson:"court_number" json:"court_number"` // Court number, e.g., 1, 2, 3
 		IsBooked        bool               `bson:"is_booked" json:"is_booked"`       // True if the court is booked
-		BookedBy        primitive.ObjectID `bson:"booked_by,omitempty" json:"booked_by,omitempty"` // ID of the user who booked
 	}
 
 	BadmintonSlot struct {
 		Id              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 		StartTime       string             `bson:"start_time" json:"start_time"`
 		EndTime         string             `bson:"end_time" json:"end_time"`
-		Courts          []BadmintonCourt   `bson:"courts" json:"courts"`             // List of courts for this slot
+		CourtId 		string				`bson:"court_id" json:"court_id"`
+		// Courts          []BadmintonCourt   `bson:"courts" json:"courts"`
 		Status          int                `bson:"status" json:"status"`
 		CreatedAt       time.Time          `bson:"created_at" json:"created_at"`
 		UpdatedAt       time.Time          `bson:"updated_at" json:"updated_at"`
