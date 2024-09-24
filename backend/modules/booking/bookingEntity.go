9603410806ba85db6a86bd29c1d7eedd545e6405
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-09-24 20:18:54 +0700

    Booking: Update Booking Entity.

--- backend/modules/booking/bookingEntity.go
+++ backend/modules/booking/bookingEntity.go
@@ -8,11 +8,13 @@ import (
 
 type (
 	Booking struct {
-		Id        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-		UserId    string             `bson:"user_id" json:"user_id"`
-		SlotId    string             `bson:"slot_id" json:"slot_id"`
-		Status    int                `bson:"status" json:"status"`
-		CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-		UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+		Id              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+		UserId          primitive.ObjectID `bson:"user_id" json:"user_id"`
+		SlotId          *primitive.ObjectID `bson:"slot_id,omitempty" json:"slot_id,omitempty"`           
+		BadmintonSlotId *primitive.ObjectID `bson:"badminton_slot_id,omitempty" json:"badminton_slot_id,omitempty"`
+		SlotType        string             `bson:"slot_type" json:"slot_type"`   
+		Status          int                `bson:"status" json:"status"`
+		CreatedAt       time.Time          `bson:"created_at" json:"created_at"`        
+		UpdatedAt       time.Time          `bson:"updated_at" json:"updated_at"`
 	}
 )
