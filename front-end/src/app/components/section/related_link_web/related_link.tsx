c651eb9915b19420aad842b3f815368083cd5728
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-09-25 23:20:36 +0700

    Update related_link.tsx

--- front-end/src/app/components/section/related_link_web/related_link.tsx
+++ front-end/src/app/components/section/related_link_web/related_link.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ralate_link = () => {
   return (
     <div className="w-full h-full p-10 flex justify-center">
       <div className="flex flex-col gap-5 items-center w-full">
         <h1 className="text-4xl font-semibold">Related Links</h1>
-        <div className="w-full">
-          <ul className="grid grid-cols-5 grid-flow-row gap-4">
-            <li className="w-[500px]">1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
+        <div className="relative w-full h-auto overflow-x-hidden">
+          <ul className="w-full h-40  grid grid-cols-5 gap-4">
+            <li className="w-full">1</li>
+            <li className="w-full ">2</li>
+            <li className="w-full ">3</li>
+            <li className="w-full">4</li>
+            <li className="w-full">5</li>
+            <li className="w-full">6</li>
+            <li className="w-full">7</li>
+            <li className="w-full">8</li>
           </ul>
         </div>
       </div>
