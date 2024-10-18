82aafba3744bbb924bf4f0eecc46a1e21ffbedf4
franz1us <franzius.won@gmail.com>
2024-10-18 22:59:23 +0700

    Football: fetch slot

--- frontend/src/app/(Booking)/football-booking/page.tsx
+++ frontend/src/app/(Booking)/football-booking/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import "./football.css";
 import Available from "@/app/assets/available.png";
 import Unavailable from "@/app/assets/unavailable.png";
-import Back from '@/app/assets/back.png'
+import Back from "@/app/assets/back.png";
 
 interface Data {
   time: string;
@@ -87,6 +87,37 @@ function Football_Booking() {
     setIsMobileView(false);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [slot, setSlot] = useState<any[]>([]);
+
+  const getSlot = async () => {
+  try {
+    const resSlot = await fetch("http://localhost:1335/facility_v1/football/slot_v1/slots", {
+      method: "GET",
+      cache: "no-store",
+    });
+
+    if (!resSlot.ok) {
+      throw new Error(`Failed to fetch: ${resSlot.statusText} (Status: ${resSlot.status})`);
+    }
+    const slotData = await resSlot.json();
+    setSlot(slotData);
+  } catch (error) {
+    console.error('Error fetching slot data:', error);
+  }
+};
+
+  useEffect(() => {
+    getSlot();
+    const intervalId = setInterval(() => {
+      getSlot();
+    }, 10000); 
+    
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <>
       <div className="flex flex-col items-center h-screen p-6">
@@ -101,7 +132,13 @@ function Football_Booking() {
               <div className="bg-white border border-gray-200 rounded-lg p-4 shadow-md">
                 <form onSubmit={handleSubmit}>
                   <div className="my-3 ">
-                    <img src={Back.src} alt="back" onClick={handleBackToTimeSlots} className="border shadow-xl p-2 rounded-md cursor-pointer hover:bg-gray-200" width={40}/>
+                    <img
+                      src={Back.src}
+                      alt="back"
+                      onClick={handleBackToTimeSlots}
+                      className="border shadow-xl p-2 rounded-md cursor-pointer hover:bg-gray-200"
+                      width={40}
+                    />
                   </div>
 
                   <label className="block mb-4">
@@ -174,6 +211,71 @@ function Football_Booking() {
             // Normal and larger screen view
             <>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {slot.length > 0 ? (
+                  slot.map((lot) => (
+                    <div key={lot._id} className="bg-white p-4 rounded shadow">
+                      <ul>
+                        <li>{lot.start_time}</li>
+                        <li>{lot.end_time}</li>
+                        <li>{lot.max_bookings}</li>
+                        <li>{lot.current_bookings}</li>
+                      </ul>
+                    </div>
+                  ))
+                ) : (
+                  <p className="bg-gray-300 p-3 my-3">No users available!</p>
+                )}
+              </div>
+
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {slot.map((lot) => (
+                  <div
+                    key={lot._id}
+                    className={`bg-white border border-gray-200 rounded-lg p-6 shadow-md transition-transform duration-300 ease-in-out
+                      ${
+                        lot.current_bookings
+                          ? "cursor-not-allowed"
+                          : "cursor-pointer hover:scale-105 hover:shadow-lg"
+                      }
+                      ${
+                        selectedCard === lot && lot.current_bookings
+                          ? ""
+                          : "bg-green-200 scale-105 shadow-lg"
+                      }
+                      ${
+                        !lot.current_bookings
+                          ? "hover:bg-green-200"
+                          : "bg-gray-300 text-gray-700"
+                      }`}
+                    onClick={() => handleCardClick(lot, lot.status)}
+                  >
+                    <div className="text-lg font-semibold flex justify-between items-center">
+                      <div>{lot.start_time} - {lot.end_time}</div>
+                      <div>
+                        {lot.current_bookings ? (
+                          
+                          <img
+                          src={Unavailable.src}
+                          alt="Unavailable"
+                          className="w-6 h-6"
+                        />
+                        ) : (
+                          <img
+                            src={Available.src}
+                            alt="Available"
+                            className="w-6 h-6"
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+
+
+              {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {data.map((item, index) => (
                   <div
                     key={index}
@@ -215,7 +317,7 @@ function Football_Booking() {
                     </div>
                   </div>
                 ))}
-              </div>
+              </div> */}
 
               <div
                 className={`hidden sm:block transition-all duration-300 ease-in-out mt-6 p-4 bg-white border border-gray-200 rounded-lg shadow-md transform ${
@@ -224,7 +326,7 @@ function Football_Booking() {
                     : "translate-y-5 opacity-0"
                 }`}
               >
-                {selectedCard !== null && data[selectedCard].isAvailable && (
+                {selectedCard !== null && slot[selectedCard].isAvailable && (
                   <>
                     <h2 className="text-xl font-bold mb-4">
                       Booking for {data[selectedCard].time}
