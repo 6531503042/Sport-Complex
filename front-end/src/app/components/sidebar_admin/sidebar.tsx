71b87b73efad99b5b0d7f7671ae3b0b25026cf99
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-10-20 22:03:55 +0700

    font-family : golbal font style using arial, sidebar : nothing

--- front-end/src/app/components/sidebar_admin/sidebar.tsx
+++ front-end/src/app/components/sidebar_admin/sidebar.tsx
@@ -16,12 +16,12 @@ type SidebarProps = {
 const sidebar: React.FC<SidebarProps> = ({ activePage }) => {
   const getActiveClass = (page: string) => {
     return activePage === page
-      ? "bg-white text-black shadow-black hover:shadow-black py-3 px-5 shadow-lg hover:shadow-2xl rounded-lg"
-      : "hover:scale-110 transition-transform duration-1000 ease-in-out ms-1 hover:shadow-lg py-3 px-5";
+      ? "border border-white text-white font-semibold shadow-gray-800 hover:shadow-black py-3 px-5 shadow-lg hover:shadow-2xl rounded-lg hover:scale-105 transition-all duration-700 hover:bg-orange-800"
+      : "hover:scale-110 transition-transform duration-1000 ease-in-out ms-1 hover:shadow-lg py-3 px-5 rounded-lg";
   };
 
   return (
-    <div className="bg-red-900 h-screen text-white md:w-[300px] w-[100px] flex flex-col px-5 py-10 ">
+    <div className="bg-red-900 h-screen text-white md:w-[300px] w-[100px] flex flex-col px-5 py-10">
       <Link href="/" className="inline-flex flex-row justify-center md:gap-3.5">
         <img src={Logo.src} alt="Logo" className="w-7 h-min" />
         <span className="flex flex-col md:border-l-2 border-l-0 w-max whitespace-nowrap font-bold">
@@ -36,13 +36,13 @@ const sidebar: React.FC<SidebarProps> = ({ activePage }) => {
             </span>
           </div>
         </span>
-      </Link>
+      </Link>   
       <br />
       <br />
       <div className="border-b"></div>
       <br />
       <br />
-      <ul className="inline-flex flex-col md:items-start items-center px-5 gap-10 font-medium text-base">
+      <ul className="inline-flex flex-col md:items-start items-center px-5 gap-10">
         <li className={getActiveClass("admin_dashboard")} style={{cursor:"pointer"}}>
           <Link
             href="/admin_dashboard"
@@ -58,7 +58,7 @@ const sidebar: React.FC<SidebarProps> = ({ activePage }) => {
             className="inline-flex flex-row items-center cursor-pointer"
           >
             <PeopleAltIcon className="md:hidden" />
-            <p className="hidden md:block">User Management</p>
+            <p className="hidden md:block">User</p>
           </Link>
         </li>
         <li className={getActiveClass("admin_facility")} style={{cursor:"pointer"}}>
