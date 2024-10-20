c1619a53459bfe495877771a00aabd9234b97424
MABiuS1 <66194783+MABiuS1@users.noreply.github.com>
2024-10-20 23:45:56 +0700

    Payment: implement payment service integration

--- backend/server/booking.go
+++ backend/server/booking.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"log"
+	client "main/client/payment"
 	"main/modules/booking/handler"
 	"main/modules/booking/repository"
 	"main/modules/booking/usecase"
@@ -9,27 +10,28 @@ import (
 
 // bookingService initializes the booking module, including scheduling the midnight clearing.
 func (s *server) bookingService() {
-    // Initialize repositories
-    bookingRepo := repository.NewBookingRepository(s.db)
+	// Initialize repositories
+	bookingRepo := repository.NewBookingRepository(s.db)
 
-    // Initialize usecases
-    bookingUsecase := usecase.NewBookingUsecase(bookingRepo)
+	// Initialize usecases
+	bookingUsecase := usecase.NewBookingUsecase(bookingRepo)
 
-    // Initialize HTTP handlers
-    bookingHttpHandler := handler.NewBookingHttpHandler(s.cfg, bookingUsecase)
+	paymentClient := client.NewPaymentClient("http://localhost:1327/payment_v1")
+	// Initialize HTTP handlers
+	bookingHttpHandler := handler.NewBookingHttpHandler(s.cfg, bookingUsecase, paymentClient)
 
-    // Schedule midnight clearing
-    // go bookingUsecase.ScheduleMidnightClearing()
-    //go
+	// Schedule midnight clearing
+	// go bookingUsecase.ScheduleMidnightClearing()
+	//go
 
-    // Booking Routes
-    booking := s.app.Group("/booking_v1")
-    // booking.POST("/bookings", bookingHttpHandler.CreateBooking) // Create a booking
-    booking.GET("/bookings/:booking_id", bookingHttpHandler.FindBooking) // Find a specific booking
-    booking.GET("/bookings/user/:user_id", bookingHttpHandler.FindOneUserBooking) // Find all bookings for a specific user
+	// Booking Routes
+	booking := s.app.Group("/booking_v1")
+	// booking.POST("/bookings", bookingHttpHandler.CreateBooking) // Create a booking
+	booking.GET("/bookings/:booking_id", bookingHttpHandler.FindBooking)          // Find a specific booking
+	booking.GET("/bookings/user/:user_id", bookingHttpHandler.FindOneUserBooking) // Find all bookings for a specific user
 
-    bookingCreate := booking.Group("/:facilityName")
-    bookingCreate.POST("/booking", bookingHttpHandler.CreateBooking) // Create a booking for a specific facility
+	bookingCreate := booking.Group("/:facilityName")
+	bookingCreate.POST("/booking", bookingHttpHandler.CreateBooking) // Create a booking for a specific facility
 
-    log.Println("Booking service initialized")
+	log.Println("Booking service initialized")
 }
