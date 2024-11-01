9dc0fd4f0ec6cc54d72e43f881b168de3d33a307
franz1us <franzius.won@gmail.com>
2024-11-01 23:54:58 +0700

    Football: Add style on slot Unavilable

--- frontend/src/app/(Booking)/football-booking/page.tsx
+++ frontend/src/app/(Booking)/football-booking/page.tsx
@@ -6,6 +6,7 @@ import NavBar from "@/app/components/navbar/navbar";
 import CheckIcon from "@mui/icons-material/Check";
 import ClearIcon from "@mui/icons-material/Clear";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ReportProblemIcon from "@mui/icons-material/ReportProblem";
 
 interface UserData {
   id: string;
@@ -172,10 +173,14 @@ setSlot(Array.isArray(slotData) && slotData.length ? slotData : []);
             
             
           {slot && slot.length === 0 ? (
-          <div className="text-center text-gray-600 text-xl">
-            Slot Unavailable!
-          </div>
-        ) : isMobileView ? (
+  <div className="slot-unavailable-card text-center p-8 rounded-lg shadow-md transition-transform duration-200 ease-in-out transform hover:scale-105">
+   <ReportProblemIcon className="slot-unavailable-icon text-red-500 mb-4" style={{ fontSize: "3rem" }} />
+    <h2 className="text-3xl font-bold text-gray-800 mb-2">Slot Unavailable</h2>
+    <p className="text-gray-600 text-lg">
+      All slots are currently booked. Please check back later or contact support for more options.
+    </p>
+  </div>
+) : isMobileView ? (
           // Mobile View: Show the form instead of the time slots
           <div className="block sm:hidden ">
             <div className="bg-white border border-gray-200 rounded-lg p-4 shadow-md">
