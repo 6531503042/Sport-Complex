f377a06670af62d202fdf3c116e035d194e426bb
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-02 22:41:27 +0700

    Update tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -18,9 +18,14 @@ const config: Config = {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
       },
       animation: {
         fade: 'fade 0.5s ease-in-out',
+        wiggle: 'wiggle 0.25s ease-in-out',
       },
     },
   },
