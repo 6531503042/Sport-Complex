cbf95216e686536bf8e3714bc28e2c5ff164507b
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-03 22:22:40 +0700

    Mongo: Remove unused line.

--- backend/pkg/utils/mongo.go
+++ backend/pkg/utils/mongo.go
@@ -2,7 +2,7 @@ package utils
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-func ConvertToObjectId (id string) primitive.ObjectID {
+func ConvertToObjectId(id string) primitive.ObjectID {
 	objectId, _ := primitive.ObjectIDFromHex(id)
 	return objectId
 }
