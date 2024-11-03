2956b77abef6406faa6fd460a7b3a0b136c2000a
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-03 17:30:45 +0700

    User: Updated userEntity variables.

--- backend/modules/user/userEntity.go
+++ backend/modules/user/userEntity.go
@@ -9,11 +9,11 @@ import (
 type (
 
 	User struct {
-		Id        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Id        primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
 		Email     string             `json:"email" bson:"email"`
 		Name      string             `json:"name" bson:"name"`
 		Password  string             `json:"password" bson:"password"`
-		CreatedAt 	time.Time          `json:"created_at" bson:"created_at"`
+		CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
 		UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
 		UserRoles  []UserRole        `bson:"user_roles"`
 	}
