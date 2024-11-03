caca7911d7499fa7792f4491e3d7ccfd5d6cd8ac
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-03 17:32:39 +0700

    Handler: Updated grpc handler.

--- backend/modules/auth/handlers/authGrpcHandler.go
+++ backend/modules/auth/handlers/authGrpcHandler.go
@@ -25,15 +25,3 @@ func (g *authGrpcHandler) AccessTokenSearch(ctx context.Context, req *authPb.Acc
 func (g *authGrpcHandler) RolesCount(ctx context.Context, req *authPb.RolesCountReq) (*authPb.RolesCountRes, error) {
 	return g.authUsecase.RolesCount(ctx)
 }
-
-// func (g *authGrpcHandler) FindOneUserProfileToRefresh(ctx context.Context, req *authPb.FindOneUserProfileToRefreshReq) (*authPb.UserProfile, error) {
-//     // Use your usecase to fetch the user profile based on the userId from the request
-//     userProfile, err := g.authUsecase.GetUserProfileByID(req.UserId)
-//     if err != nil {
-//         return nil, status.Errorf(codes.NotFound, "user profile not found: %v", err)
-//     }
-//     return &authPb.UserProfile{
-//         UserId:   userProfile.ID,
-//         Username: userProfile.Username, // Adjust according to your User model
-//     }, nil
-// }
