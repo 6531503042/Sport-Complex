8abef0c5020deafdd377f62e25e200196b728fe2
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-05 01:15:52 +0700

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,21 @@
 "use client";
 import React from 'react';
 import { useAuth } from '../app/context/AuthContext';
+import HomePage from '@/app/(PagesPacking)/homepage/page'
+import LoginPage from './(PagesPacking)/login/page';
 
-const HomePage = () => {
+const Main = () => {
   const { user, logout } = useAuth();
 
   return (
     <div>
       {user ? (
-        <div>
-          <h1>Welcome, {user.name}</h1>
-          <button onClick={logout}>Logout</button>
-        </div>
+        <HomePage/>
       ) : (
-        <p>Loading...</p>
+        <LoginPage/>
       )}
     </div>
   );
 };
 
-export default HomePage;
+export default Main;
