0144d87abbbe8d0f9829b492f0ec53034bf9456b
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-05 01:19:30 +0700

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from 'react';
 import { useAuth } from '../app/context/AuthContext';
-import HomePage from '@/app/(PagesPacking)/homepage/page'
-import LoginPage from './(PagesPacking)/login/page';
+import HomePage from '@/app/(pages)/homepage/page'
+import LoginPage from './(pages)/login/page';
 
 const Main = () => {
   const { user, logout } = useAuth();
