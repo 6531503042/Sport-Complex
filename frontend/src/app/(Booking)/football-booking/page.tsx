38f0ac8719a9bbc0ad723d41bbd52999b699249c
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-05 16:03:55 +0700

    add activepage effect

--- frontend/src/app/(Booking)/football-booking/page.tsx
+++ frontend/src/app/(Booking)/football-booking/page.tsx
@@ -5,7 +5,7 @@ import "./football.css";
 import NavBar from "@/app/components/navbar/navbar";
 import CheckIcon from "@mui/icons-material/Check";
 import ClearIcon from "@mui/icons-material/Clear";
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ReportProblemIcon from "@mui/icons-material/ReportProblem";
 
 interface UserData {
@@ -45,7 +45,7 @@ function Football_Booking({ params }: UserDataParams) {
     e.preventDefault(); // Prevent default form submission
 
     // Form validation: Check if the phone number is filled in
-    
+
     try {
       // Ensure the selectedCard is valid before proceeding
       if (selectedCard === null || !slot[selectedCard]) {
@@ -106,7 +106,7 @@ function Football_Booking({ params }: UserDataParams) {
   };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const [slot, setSlot] = useState<any[] | null>(null);
+  const [slot, setSlot] = useState<any[] | null>(null);
   const getSlot = async () => {
     try {
       const resSlot = await fetch(
@@ -116,15 +116,15 @@ const [slot, setSlot] = useState<any[] | null>(null);
           cache: "no-store",
         }
       );
-  
+
       if (!resSlot.ok) {
         throw new Error(
           `Failed to fetch: ${resSlot.statusText} (Status: ${resSlot.status})`
         );
       }
-  
+
       const slotData = await resSlot.json();
-setSlot(Array.isArray(slotData) && slotData.length ? slotData : []);
+      setSlot(Array.isArray(slotData) && slotData.length ? slotData : []);
     } catch (error) {
       console.error("Error fetching slot data:", error);
       setSlot([]); // Set to empty array if there's an error fetching
@@ -140,7 +140,7 @@ setSlot(Array.isArray(slotData) && slotData.length ? slotData : []);
       setFormData((prevData) => ({
         ...prevData,
         name: user.name || "",
-      id: user.id.replace(/^user:/, "") || "", // Remove "user:" prefix if it exists
+        id: user.id.replace(/^user:/, "") || "", // Remove "user:" prefix if it exists
       }));
     }
     const userDataId = localStorage.getItem("_id");
@@ -164,114 +164,124 @@ setSlot(Array.isArray(slotData) && slotData.length ? slotData : []);
 
   return (
     <>
-      <NavBar />
+      <NavBar activePage="football"/>
       <div className="flex flex-col items-center h-screen p-6">
         <div className="w-full max-w-[1189px] bg-[#FEFFFE] border-gray border rounded-3xl drop-shadow-2xl p-5">
           <h1 className="text-4xl font-bold my-10 text-black text-center">
             Football Booking
           </h1>
-            
-            
+
           {slot && slot.length === 0 ? (
-  <div className="slot-unavailable-card text-center p-8 rounded-lg shadow-md transition-transform duration-200 ease-in-out transform hover:scale-105">
-   <ReportProblemIcon className="slot-unavailable-icon text-red-500 mb-4" style={{ fontSize: "3rem" }} />
-    <h2 className="text-3xl font-bold text-gray-800 mb-2">Slot Unavailable</h2>
-    <p className="text-gray-600 text-lg">
-      All slots are currently booked. Please check back later or contact support for more options.
-    </p>
-  </div>
-) : isMobileView ? (
-          // Mobile View: Show the form instead of the time slots
-          <div className="block sm:hidden ">
-            <div className="bg-white border border-gray-200 rounded-lg p-4 shadow-md">
-              <form onSubmit={handleSubmit}>
-                {/* Flex container for aligning back button and time at the top */}
-                <div className="flex items-center justify-between my-3">
-                  <ArrowBackIosNewIcon
-                    className="border shadow-xl w-10 h-10 p-2 rounded-md cursor-pointer hover:bg-gray-200"
-                    onClick={handleBackToTimeSlots}
-                    style={{ fontSize: "2rem" }}
-                  />
-                  {selectedCard !== null && slot[selectedCard] && (
-                    <h2 className="text-xl font-semibold text-start">
-                      {slot[selectedCard].start_time} -{" "}
-                      {slot[selectedCard].end_time}
-                    </h2>
-                  )}
-                </div>
-
-                <label className="block mb-4">
-                  <span className="block text-sm font-medium text-gray-700 py-2">
-                    Name
-                  </span>
-                  <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    readOnly
-                    className="name-input-football mt-1 block w-full px-3 py-3"
-                  />
-                </label>
-
-                <label className="block mb-4">
-                  <span className="block text-sm font-medium text-gray-700 py-2">
-                    Lecturer / Staff / Student ID
-                  </span>
-                  <input
-                    type="text"
-                    name="id"
-                    value={formData.id}
-                    readOnly
-                    className="name-input-football mt-1 block w-full px-3 py-3"
-                  />
-                </label>
-
-             
-
-                {/* Center the Booking button */}
-                <div className="flex justify-center">
-                  <button
-                    type="submit"
-                    className="font-bold bg-[#5EB900] text-white px-5 py-2.5 rounded-md drop-shadow-2xl hover:bg-[#005400]"
-                  >
-                    Booking
-                  </button>
-                </div>
-              </form>
+            <div className="slot-unavailable-card text-center p-8 rounded-lg shadow-md transition-transform duration-200 ease-in-out transform hover:scale-105">
+              <ReportProblemIcon
+                className="slot-unavailable-icon text-red-500 mb-4"
+                style={{ fontSize: "3rem" }}
+              />
+              <h2 className="text-3xl font-bold text-gray-800 mb-2">
+                Slot Unavailable
+              </h2>
+              <p className="text-gray-600 text-lg">
+                All slots are currently booked. Please check back later or
+                contact support for more options.
+              </p>
             </div>
-          </div>
-        )
+          ) : isMobileView ? (
+            // Mobile View: Show the form instead of the time slots
+            <div className="block sm:hidden ">
+              <div className="bg-white border border-gray-200 rounded-lg p-4 shadow-md">
+                <form onSubmit={handleSubmit}>
+                  {/* Flex container for aligning back button and time at the top */}
+                  <div className="flex items-center justify-between my-3">
+                    <ArrowBackIosNewIcon
+                      className="border shadow-xl w-10 h-10 p-2 rounded-md cursor-pointer hover:bg-gray-200"
+                      onClick={handleBackToTimeSlots}
+                      style={{ fontSize: "2rem" }}
+                    />
+                    {selectedCard !== null && slot[selectedCard] && (
+                      <h2 className="text-xl font-semibold text-start">
+                        {slot[selectedCard].start_time} -{" "}
+                        {slot[selectedCard].end_time}
+                      </h2>
+                    )}
+                  </div>
 
+                  <label className="block mb-4">
+                    <span className="block text-sm font-medium text-gray-700 py-2">
+                      Name
+                    </span>
+                    <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      readOnly
+                      className="name-input-football mt-1 block w-full px-3 py-3"
+                    />
+                  </label>
 
-           : (
+                  <label className="block mb-4">
+                    <span className="block text-sm font-medium text-gray-700 py-2">
+                      Lecturer / Staff / Student ID
+                    </span>
+                    <input
+                      type="text"
+                      name="id"
+                      value={formData.id}
+                      readOnly
+                      className="name-input-football mt-1 block w-full px-3 py-3"
+                    />
+                  </label>
+
+                  {/* Center the Booking button */}
+                  <div className="flex justify-center">
+                    <button
+                      type="submit"
+                      className="font-bold bg-[#5EB900] text-white px-5 py-2.5 rounded-md drop-shadow-2xl hover:bg-[#005400]"
+                    >
+                      Booking
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          ) : (
             // Normal and larger screen view
             <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {slot?.map((lot) => (
-                <div
-                  key={lot._id}
-                  className={`border border-gray-200 rounded-lg p-6 shadow-md transition-transform duration-300 ease-in-out
-                    ${lot.current_bookings ? "cursor-not-allowed bg-[#C1C7D4] text-white" : "cursor-pointer bg-[#5EB900] text-white border-green-300 hover:scale-105 hover:shadow-lg"}
-                    ${!lot.current_bookings ? "hover:bg-[#005400]" : ""}`}
-                  onClick={() => {
-                    if (!lot.current_bookings) {
-                      handleCardClick(lot);
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {slot?.map((lot) => (
+                  <div
+                    key={lot._id}
+                    className={`border border-gray-200 rounded-lg p-6 shadow-md transition-transform duration-300 ease-in-out
+                    ${
+                      lot.current_bookings
+                        ? "cursor-not-allowed bg-[#C1C7D4] text-white"
+                        : "cursor-pointer bg-[#5EB900] text-white border-green-300 hover:scale-105 hover:shadow-lg"
                     }
-                  }}
-                >
-                  <div className="text-lg font-semibold flex justify-between items-center">
-                    <div>
-                      {lot.start_time} - {lot.end_time}
-                    </div>
-                    <div>
-                      {lot.current_bookings ? (
-                        <ClearIcon className="mx-2.5" style={{ fontSize: "1.3rem" }} />
-                      ) : (
-                        <CheckIcon className="mx-2.5" style={{ fontSize: "1.3rem" }} />
-                      )}
+                    ${!lot.current_bookings ? "hover:bg-[#005400]" : ""}`}
+                    onClick={() => {
+                      if (!lot.current_bookings) {
+                        handleCardClick(lot);
+                      }
+                    }}
+                  >
+                    <div className="text-lg font-semibold flex justify-between items-center">
+                      <div>
+                        {lot.start_time} - {lot.end_time}
+                      </div>
+                      <div>
+                        {lot.current_bookings ? (
+                          <ClearIcon
+                            className="mx-2.5"
+                            style={{ fontSize: "1.3rem" }}
+                          />
+                        ) : (
+                          <CheckIcon
+                            className="mx-2.5"
+                            style={{ fontSize: "1.3rem" }}
+                          />
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
                 ))}
               </div>
 
