3ca692ee1f2e29977ca746dab4a5b0f31170c5c4
Parking <122780034+kongphop1209@users.noreply.github.com>
2024-11-11 14:14:57 +0700

    Update useAuthRedirect.ts

--- frontend/src/app/components/hooks/useAuthRedirect.ts
+++ frontend/src/app/components/hooks/useAuthRedirect.ts
@@ -1,37 +1,17 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import axios from "axios";
-import { getToken } from "@/app/utils/auth";
 
 const useAuthRedirect = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const checkAuth = async () => {
-      const token = getToken();
+    // Check if there is user data stored in localStorage
+    const userData = localStorage.getItem("user");
 
-      if (!token) {
-        router.replace("/login");
-        return;
-      }
-
-      try {
-        const response = await axios.get("/api/auth/check-auth", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.status !== 200) {
-          router.replace("/login");
-        }
-      } catch (error) {
-        console.error("Authorization check failed:", error);
-        router.replace("/login");
-      }
-    };
-
-    checkAuth();
+    // If there's no user data (i.e., not logged in), redirect to login page
+    if (!userData) {
+      router.replace("/login");
+    }
   }, [router]);
 };
 
