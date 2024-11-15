306177e1593697934fd4314c7b0c8b328afea611
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-15 22:16:54 +0700

    Mongo: Updated adding NewMongoDBConnection new func.

--- backend/pkg/utils/mongo.go
+++ backend/pkg/utils/mongo.go
@@ -1,8 +1,33 @@
 package utils
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func ConvertToObjectId(id string) primitive.ObjectID {
 	objectId, _ := primitive.ObjectIDFromHex(id)
 	return objectId
+}
+
+func NewMongoDBConnection(pctx context.Context, uri string) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(pctx, 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().
+        ApplyURI(uri).
+        SetMaxPoolSize(100).
+        SetMinPoolSize(10).
+        SetMaxConnIdleTime(30 * time.Second)
+
+    client, err := mongo.Connect(ctx, clientOptions)
+    if err != nil {
+        return nil, err
+    }
+
+    return client, nil
 }
