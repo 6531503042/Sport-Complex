e08752ed2d5af99674f5b69ba1e226c4d84cfa01
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-15 22:15:32 +0700

    Booking: Updated RoleCode

--- backend/modules/user/userModel.go
+++ backend/modules/user/userModel.go
@@ -7,19 +7,21 @@ type (
 		Id        string    `json:"id"`
 		Email     string    `json:"email"`
 		Name      string    `json:"name"`
+		RoleCode  int       `json:"role_code"`
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
 	}
 
 	UserClaims struct {
 		Id       string `json:"id"`
-		RoleCode []int  `json:"role_code"`
+		RoleCode int    `json:"role_code"`
 	}
 
 	CreateUserReq struct {
 		Email    string `json:"email" form:"email" validate:"required,email,max=255"`
 		Password string `json:"password" form:"password" validate:"required,max=32"`
 		Name     string `json:"name" form:"name" validate:"required,max=32"`
+		RoleCode int    `json:"role_code" form:"role_code"`
 	}
 	
 )
