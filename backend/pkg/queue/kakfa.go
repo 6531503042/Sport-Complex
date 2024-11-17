9c52e5178b43dcb0d5d17f1693d1d40d45763d56
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-17 20:46:23 +0700

    Kafka: Comment unused kafka in pkg.

--- backend/pkg/queue/kakfa.go
+++ backend/pkg/queue/kakfa.go
@@ -1,104 +1,104 @@
 package queue
 
-import (
-	"crypto/tls"
-	"encoding/json"
-	"errors"
-	"log"
+// import (
+// 	"crypto/tls"
+// 	"encoding/json"
+// 	"errors"
+// 	"log"
 
-	"github.com/IBM/sarama"
-	"github.com/go-playground/validator"
-)
+// 	"github.com/IBM/sarama"
+// 	"github.com/go-playground/validator"
+// )
 
-func ConnectProducer(brokerUrls []string, apiKey, secret string) (sarama.SyncProducer, error) {
-	config := sarama.NewConfig()
-	if apiKey != "" && secret != "" {
-		config.Net.SASL.Enable = true
-		config.Net.SASL.User = apiKey
-		config.Net.SASL.Password = secret
-		config.Net.SASL.Mechanism = "PLAIN"
-		config.Net.SASL.Handshake = true
-		config.Net.SASL.Version = sarama.SASLHandshakeV1
-		config.Net.TLS.Enable = true
-		config.Net.TLS.Config = &tls.Config{
-			InsecureSkipVerify: true,
-			ClientAuth: tls.NoClientCert,
-		}
-	}  
-	config.Producer.Return.Successes = true
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	config.Producer.Retry.Max = 3
+// func ConnectProducer(brokerUrls []string, apiKey, secret string) (sarama.SyncProducer, error) {
+// 	config := sarama.NewConfig()
+// 	if apiKey != "" && secret != "" {
+// 		config.Net.SASL.Enable = true
+// 		config.Net.SASL.User = apiKey
+// 		config.Net.SASL.Password = secret
+// 		config.Net.SASL.Mechanism = "PLAIN"
+// 		config.Net.SASL.Handshake = true
+// 		config.Net.SASL.Version = sarama.SASLHandshakeV1
+// 		config.Net.TLS.Enable = true
+// 		config.Net.TLS.Config = &tls.Config{
+// 			InsecureSkipVerify: true,
+// 			ClientAuth: tls.NoClientCert,
+// 		}
+// 	}
+// 	config.Producer.Return.Successes = true
+// 	config.Producer.RequiredAcks = sarama.WaitForAll
+// 	config.Producer.Retry.Max = 3
 
-	producer, err := sarama.NewSyncProducer(brokerUrls, config)
-	if err != nil {
-		log.Printf("Error: Failed to connect to producer: %s", err.Error())
-		return nil, errors.New("error: failed to connect to producer")
-	}
-	return producer, nil
-}
+// 	producer, err := sarama.NewSyncProducer(brokerUrls, config)
+// 	if err != nil {
+// 		log.Printf("Error: Failed to connect to producer: %s", err.Error())
+// 		return nil, errors.New("error: failed to connect to producer")
+// 	}
+// 	return producer, nil
+// }
 
-func ConnectConsumer(brokerUrls []string, apiKey, secret string) (sarama.Consumer, error) {
-	config := sarama.NewConfig()
-	if apiKey != "" && secret != "" {
-		config.Net.SASL.Enable = true
-		config.Net.SASL.User = apiKey
-		config.Net.SASL.Password = secret
-		config.Net.SASL.Mechanism = "PLAIN"
-		config.Net.SASL.Handshake = true
-		config.Net.SASL.Version = sarama.SASLHandshakeV1
-		config.Net.TLS.Enable = true
-		config.Net.TLS.Config = &tls.Config{
-			InsecureSkipVerify: true,
-			ClientAuth: tls.NoClientCert,
-		}
-	}
-	config.Consumer.Return.Errors = true
-	config.Consumer.Fetch.Max = 3
+// func ConnectConsumer(brokerUrls []string, apiKey, secret string) (sarama.Consumer, error) {
+// 	config := sarama.NewConfig()
+// 	if apiKey != "" && secret != "" {
+// 		config.Net.SASL.Enable = true
+// 		config.Net.SASL.User = apiKey
+// 		config.Net.SASL.Password = secret
+// 		config.Net.SASL.Mechanism = "PLAIN"
+// 		config.Net.SASL.Handshake = true
+// 		config.Net.SASL.Version = sarama.SASLHandshakeV1
+// 		config.Net.TLS.Enable = true
+// 		config.Net.TLS.Config = &tls.Config{
+// 			InsecureSkipVerify: true,
+// 			ClientAuth: tls.NoClientCert,
+// 		}
+// 	}
+// 	config.Consumer.Return.Errors = true
+// 	config.Consumer.Fetch.Max = 3
 
-	consumer, err := sarama.NewConsumer(brokerUrls, config)
-	if err != nil {
-		log.Printf("Error: Failed to connect to consumer: %s", err.Error())
-		return nil, errors.New("error: failed to connect to consumer")
-	}
+// 	consumer, err := sarama.NewConsumer(brokerUrls, config)
+// 	if err != nil {
+// 		log.Printf("Error: Failed to connect to consumer: %s", err.Error())
+// 		return nil, errors.New("error: failed to connect to consumer")
+// 	}
 
-	return consumer, nil
-}
+// 	return consumer, nil
+// }
 
-func PushMessageWithKeyToQueue(brokerUrls []string, apiKey, secret, topic, key string, message []byte) error {
-	producer, err := ConnectProducer(brokerUrls, apiKey, secret)
-	if err != nil {
-		log.Printf("Error: Failed to connect to producer: %s", err.Error())
-		return errors.New("error: failed to connect to producer")
-	}
-	defer producer.Close()
+// func PushMessageWithKeyToQueue(brokerUrls []string, apiKey, secret, topic, key string, message []byte) error {
+// 	producer, err := ConnectProducer(brokerUrls, apiKey, secret)
+// 	if err != nil {
+// 		log.Printf("Error: Failed to connect to producer: %s", err.Error())
+// 		return errors.New("error: failed to connect to producer")
+// 	}
+// 	defer producer.Close()
 
-	msg := &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.StringEncoder(message),
-		Key:   sarama.StringEncoder(key),
-	}
+// 	msg := &sarama.ProducerMessage{
+// 		Topic: topic,
+// 		Value: sarama.StringEncoder(message),
+// 		Key:   sarama.StringEncoder(key),
+// 	}
 
-	partition, offset, err := producer.SendMessage(msg)
-	if err != nil {
-		log.Printf("Error: Failed to push message to queue: %s", err.Error())
-		return errors.New("error: failed to push message to queue")
-	}
+// 	partition, offset, err := producer.SendMessage(msg)
+// 	if err != nil {
+// 		log.Printf("Error: Failed to push message to queue: %s", err.Error())
+// 		return errors.New("error: failed to push message to queue")
+// 	}
 
-	log.Printf("Message pushed to queue: partition=%d, offset=%d", partition, offset)
-	return nil
-}
+// 	log.Printf("Message pushed to queue: partition=%d, offset=%d", partition, offset)
+// 	return nil
+// }
 
-func DecodeMessage(obj any, value []byte) error {
-	if err := json.Unmarshal(value, &obj); err != nil {
-		log.Printf("Error: Failed to decode message: %s", err.Error())
-		return errors.New("error: failed to decode message")
-	}
+// func DecodeMessage(obj any, value []byte) error {
+// 	if err := json.Unmarshal(value, &obj); err != nil {
+// 		log.Printf("Error: Failed to decode message: %s", err.Error())
+// 		return errors.New("error: failed to decode message")
+// 	}
 
-	validate := validator.New()
-	if err := validate.Struct(obj); err != nil {
-		log.Printf("Error: Failed to validate message: %s", err.Error())
-		return errors.New("error: failed to validate message")
-	}
+// 	validate := validator.New()
+// 	if err := validate.Struct(obj); err != nil {
+// 		log.Printf("Error: Failed to validate message: %s", err.Error())
+// 		return errors.New("error: failed to validate message")
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
