ad713f1c2af97b3583576d34f913a53896f7f8eb
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-18 23:01:10 +0700

    Update userEntity.go

--- backend/modules/user/userEntity.go
+++ backend/modules/user/userEntity.go
@@ -12,10 +12,10 @@ type (
 		Id        primitive.ObjectID  `json:"_id" bson:"_id,omitempty"`
 		Email     string             `json:"email" bson:"email"`
 		Name      string             `json:"name" bson:"name"`
-		Password  string             `json:"password" bson:"password"`
+		Password  string             `json:"-" bson:"password"`
 		CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
 		UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
-		UserRoles  []UserRole        `bson:"user_roles"`
+		UserRoles  []UserRole        `json:"user_roles" bson:"user_roles"`
 	}
 
 	UserRole struct {
@@ -24,11 +24,13 @@ type (
 	}
 
 	UserProfileBson struct {
-		Id        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-		Email     string             `json:"email" bson:"email"`
-		Name      string             `json:"name" bson:"name"`
-		CreatedAt time.Time          `json:"created_at" bson:"created_at"`
-		UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
-		RoleCode  int                `json:"role_code" bson:"role_code"`
+		Id         primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+		Email      string            `json:"email" bson:"email"`
+		Name       string            `json:"name" bson:"name"`
+		RoleCode   int               `json:"role_code" bson:"role_code"`
+		RoleTitle  string            `json:"role_title" bson:"role_title"`
+		CreatedAt  time.Time         `json:"created_at" bson:"created_at"`
+		UpdatedAt  time.Time         `json:"updated_at" bson:"updated_at"`
+		UserRoles  []UserRole        `json:"user_roles" bson:"user_roles"`
 	}
 )
