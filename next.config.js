4fd7d5f9f91c3f85b341565b31a44f6ab40efd01
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-19 23:44:00 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig 
