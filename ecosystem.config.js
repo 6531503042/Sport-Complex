e5ae1bc1d96cf37ab8a76e887907149e0eefc1dd
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-11-28 16:14:22 +0700

    docs: update README

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,17 @@ module.exports = {
         name: "auth-service",
         script: "go",
         args: "run main.go ./env/dev/.env.auth",
-        cwd: "/Users/mabius/Sport-Complex-2/backend", // Replace with your actual backend path
+        cwd: "/Users/bricksocoolxd/GitHub-Initial/Sport-Complex/backend", // Replace with your actual backend path
         env: {
           NODE_ENV: "development",
         },
       },
+      
       {
         name: "booking-service",
         script: "go",
         args: "run main.go ./env/dev/.env.booking",
-        cwd: "/Users/mabius/Sport-Complex-2/backend", // Replace with your actual backend path
+        cwd: "/Users/bricksocoolxd/GitHub-Initial/Sport-Complex/backend", // Replace with your actual backend path
         env: {
           NODE_ENV: "development",
         },
@@ -22,7 +23,7 @@ module.exports = {
         name: "user-service",
         script: "go",
         args: "run main.go ./env/dev/.env.user",
-        cwd: "/Users/mabius/Sport-Complex-2/backend", // Replace with your actual backend path
+        cwd: "/Users/bricksocoolxd/GitHub-Initial/Sport-Complex/backend", // Replace with your actual backend path
         env: {
           NODE_ENV: "development",
         },
@@ -31,7 +32,7 @@ module.exports = {
         name: "facility-service",
         script: "go",
         args: "run main.go ./env/dev/.env.facility",
-        cwd: "/Users/mabius/Sport-Complex-2/backend", // Replace with your actual backend path
+        cwd: "/Users/bricksocoolxd/GitHub-Initial/Sport-Complex/backend", // Replace with your actual backend path
         env: {
           NODE_ENV: "development",
         },
@@ -40,7 +41,7 @@ module.exports = {
         name: "payment-service",
         script: "go",
         args: "run main.go ./env/dev/.env.payment",
-        cwd: "/Users/mabius/Sport-Complex-2/backend", // Replace with your actual backend path
+        cwd: "/Users/bricksocoolxd/GitHub-Initial/Sport-Complex/backend", // Replace with your actual backend path
         env: {
           NODE_ENV: "development",
         },
